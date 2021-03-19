60a478fe589d5d2eacfd909baf3fe17641f73c4a
noetits <noe.tits@gmail.com>
2021-03-19 10:45:22 +0100

    just test 3 basis modules with default values (one recording)

--- test_modules.py
+++ test_modules.py
@@ -2,3 +2,9 @@ from speech_tech import *
 def test_wordStress():
     bin=wordStress()
     print(bin)
+
+def test_sentenceStress():
+    sentenceStress()
+
+def test_iConstrast():
+    iConstrast()
