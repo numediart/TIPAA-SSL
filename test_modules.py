60b20f5379201df5b6cd915f9a2475126f718479
noetits <noe.tits@gmail.com>
2021-03-30 16:55:25 +0200

    unit tests for new functions

--- test_modules.py
+++ test_modules.py
@@ -1,16 +1,29 @@
 from speech_tech import *
 def test_wordStress():
-    bin=wordStress()
-    print(bin)
+    r=wordStress()
+    p=set_params(sentenceID=111, waveFileAddress='audio_recordings/SS_1_i_would_love_to_go_to_ireland.wav', module='wordStress')
+    r=wordStress(p)
 
 def test_sentenceStress():
     sentenceStress()
+    sentenceStress(p=set_params(sentenceID=1, waveFileAddress='audio_recordings/WS_111_toothpaste.wav', module='sentenceStress'))
 
-def test_iConstrast():
-    iConstrast()
+def test_iContrast():
+    iContrast()
 
 def test_chunking():
     chunking()
 
 def test_edAnalysis():
     edAnalysis()
+
+from label_data_processing import *
+
+def test_label_data_processing():
+    get_wordStress_annotation()
+    get_sentenceStress_annotation()
+    ed_make_grammars()
+
+from text_processing import word_stress_from_text
+def test_text_processing():
+    word_stress_from_text()
