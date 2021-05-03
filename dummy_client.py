ca88b4496b018c29e14a358e413aec1898b2ac0f
noetits <noe.tits@gmail.com>
2021-05-03 10:42:22 +0200

    make python client compatible with current server. Both send audio and call module work Will be easier to test new modules

--- dummy_client.py
+++ dummy_client.py
@@ -1,15 +1,9 @@
-# import requests
-# def send_audio():
-#     #print('attempting to send audio')
-#     url = 'http://127.0.0.1:5000/'
-#     with open('audio_recordings/WS_111_toothpaste.wav', 'rb') as file:
-#         data = {'uuid':'-jx-1', 'alarmType':1, 'timeDuration':10}
-#         files = {'messageFile': file}
-
-#         req = requests.post(url, files=files, json=data)
-#         print(req.status_code)
-#         print(req.text)
-
 import requests
-res = requests.post('http://localhost:5000/api/add_message/sentenceStress', json={"filename":"SS_1_i_would_love_to_go_to_ireland.wav", "sentenceID":1})
-print(res.__dict__)
+def send_audio(path='audio_recordings/WS_111_toothpaste.wav', url = 'http://localhost:5000/'):
+    with open(path, 'rb') as file:
+        files = {'file': file}
+        req = requests.post(url, files=files)
+
+def call_module(module='sentenceStress', filename='SS_1_i_would_love_to_go_to_ireland.wav', sentenceID=1):
+    res = requests.post('http://localhost:5000/flowspeech/'+module, {"data[sentenceID]":str(sentenceID), 'data[filename]':filename})
+    print(res.__dict__['_content'])
