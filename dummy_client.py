87cf35aadb048d0ba99c7c930147d06c3701190f
noetits <noe.tits@gmail.com>
2021-05-07 17:36:26 +0200

    client post requests examples for vowel stresses and phoneme contrast

--- dummy_client.py
+++ dummy_client.py
@@ -5,5 +5,13 @@ def send_audio(path='audio_recordings/WS_111_toothpaste.wav', url = 'http://loca
         req = requests.post(url, files=files)
 
 def call_module(module='sentenceStress', filename='SS_1_i_would_love_to_go_to_ireland.wav', sentenceID=1):
-    res = requests.post('http://localhost:5000/flowspeech/'+module, {"data[sentenceID]":str(sentenceID), 'data[filename]':filename})
+    res = requests.post('http://localhost:5000/flowspeech/'+module, {"sentenceID":str(sentenceID), 'filename':filename})
+    print(res.__dict__['_content'])
+
+def call_vowel_stresses(filename='SS_1_i_would_love_to_go_to_ireland.wav', text='I would love to go to ireland !'):
+    res = requests.post('http://localhost:5000/vowel_stresses/', {"text":text, 'filename':filename})
+    print(res.__dict__['_content'])
+
+def call_phoneme_contrast(filename='turned_around.mp3', text='turned around', word_id=0):
+    res = requests.post('http://localhost:5000/phonemeContrast/', {"text":text, 'filename':filename, 'word_id':word_id})
     print(res.__dict__['_content'])
