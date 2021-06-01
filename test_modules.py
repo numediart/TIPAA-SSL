6228f1bb08eb29cef9a0df518379f533070fcd21
noetits <noe.tits@gmail.com>
2021-06-01 05:10:41 +0200

    unit tests: added functions calls of annotation files automatic generations

--- test_modules.py
+++ test_modules.py
@@ -1,4 +1,9 @@
 from speech_tech import *
+
+from text_processing import word_stress_from_text
+from module_performance import *
+from label_data_processing import *
+
 def test_wordStress():
     r=wordStress()
     p=set_params(sentenceID=111, waveFileAddress='audio_recordings/SS_1_i_would_love_to_go_to_ireland.wav', module='wordStress')
@@ -25,30 +30,35 @@ def test_chunking():
 def test_edAnalysis():
     edAnalysis()
 
-from label_data_processing import *
-def test_label_data_processing():
-    get_wordStress_annotation()
-    get_sentenceStress_annotation()
-    # ed_make_grammars()
+def test_vowel_stresses():
+    vowel_stresses_from_phonetics_audio()
+    vowel_stresses_from_text_audio()
 
-from text_processing import word_stress_from_text
+def test_phonemeContrast():
+    phonemeContrast_from_text_audio()
+
+
 def test_text_processing():
     word_stress_from_text()
 
-from module_performance import *
 def test_performance_tests():
     iContrast_performance_test()
     sentenceStress_performance_test()
-
     wordStress_performance_test()
     sentenceStress_automatic_annot_performance_test()
     edAnalysis_performance_test()
     edAnalysis_from_audiobook_data(data_set='dev-clean', n=100)
     pContrast_from_audiobook_data(n=100)
 
-from label_data_processing import *
 def test_label_data_processing():
+    get_wordStress_annotation()
+    get_sentenceStress_annotation()
+    # ed_make_grammars()
     # get_data()
-    make_generic_dct_from_text()
+    make_dct_all_phones_from_text()
     make_generic_dct_from_phonetics()
     make_grammar_from_dct()
+    make_all_phones_annotation_files()
+    make_all_phones_annotation_files_from_phonetics()
+    make_pContrast_annotation_files_from_phonetics()
+    make_pContrast_annotation_files()
