6f3df655f300ecb4309fe5c63597fab6eb7f7112
noetits <noe.tits@gmail.com>
2021-06-15 16:36:35 +0200

    I put the requests module as a default "client" parameter. This would be to optionnally use a flask client instead

--- dummy_client.py
+++ dummy_client.py
@@ -1,37 +1,37 @@
 import requests
-def send_audio(path='audio_recordings/WS_111_toothpaste.wav', url = 'http://localhost:5000/'):
+def send_audio(path='audio_recordings/WS_111_toothpaste.wav', url = 'http://localhost:5000/', client=requests):
     with open(path, 'rb') as file:
         files = {'file': file}
-        req = requests.post(url, files=files)
-        # print(req.__dict__['_content'])
+        req = client.post(url, files=files)
     return req
 
-def call_module(module='sentenceStress', filename='SS_1_i_would_love_to_go_to_ireland.wav', sentenceID=1):
-    res = requests.post(url='http://localhost:5000/flowspeech/'+module, data={"sentenceID":str(sentenceID), 'filename':filename})
+def call_module(module='sentenceStress', filename='SS_1_i_would_love_to_go_to_ireland.wav', sentenceID=1, client=requests):
+    res = client.post('http://localhost:5000/flowspeech/'+module, data={"sentenceID":str(sentenceID), 'filename':filename})
     print(res.__dict__['_content'])
     return res.__dict__['_content']
 
 
-def call_vowel_stresses(filename='SS_1_i_would_love_to_go_to_ireland.wav', text='I would love to go to ireland !'):
-    res = requests.post(url='http://localhost:5000/vowel_stresses', data={"text":text, 'filename':filename})
+def call_vowel_stresses(filename='SS_1_i_would_love_to_go_to_ireland.wav', text='I would love to go to ireland !', client=requests):
+    res = client.post('http://localhost:5000/vowel_stresses', data={"text":text, 'filename':filename})
     print(res.__dict__['_content'])
     return res.__dict__['_content']
 
-
-def call_phoneme_contrast(filename='turned_around.mp3', text='turned around', word_id=0, target='D', alternatives="['T', 'D', 'T AH0', 'D AH0', 'IH0 D', 'IH1 D', 'IH2 D', 'EH2 D', 'AH0 D']"):
-    res = requests.post(url='http://localhost:5000/phonemeContrast', data={"text":text, 'filename':filename, 'word_id':word_id, 'alternatives':alternatives, 'target':target})
+def call_phoneme_contrast(filename='turned_around.mp3', text='turned around', word_id=0, target='D', alternatives="['T', 'D', 'T AH0', 'D AH0', 'IH0 D', 'IH1 D', 'IH2 D', 'EH2 D', 'AH0 D']", client=requests):
+    res = client.post('http://localhost:5000/phonemeContrast', data={"text":text, 'filename':filename, 'word_id':word_id, 'alternatives':alternatives, 'target':target})
     print(res.__dict__['_content'])
     return res.__dict__['_content']
 
 if __name__ == "__main__":
     send_audio()
     call_module()
+    call_module('hihuhiha')
     call_module(module='wordStress', filename='WS_111_toothpaste.wav', sentenceID=111)
 
     send_audio(path='audio_recordings/iC_111_slip.wav')
 
     call_module(sentenceID=111, filename='iC_111_slip.wav', module="iContrast")
     call_vowel_stresses()
+    call_vowel_stresses(filename='iC_111_sleep.wav', text='sleep')
 
     send_audio('audio_recordings/turned_around.mp3')
     call_phoneme_contrast()
@@ -42,4 +42,11 @@ if __name__ == "__main__":
 
     send_audio('audio_recordings/iC_112_leave.wav')
     alternatives="['IH0', 'IY0','IH1', 'IY1','IH2', 'IY2']"
-    call_phoneme_contrast(filename='iC_112_leave.wav', text='leave',  word_id=0, alternatives=alternatives)
+    call_phoneme_contrast(filename='iC_112_leave.wav', text='leave',  word_id=0, target='IY1', alternatives=alternatives)
+    
+    send_audio('audio_recordings/ed_acceptEED.wav')
+    # alternatives="['IH0', 'IY0','IH1', 'IY1','IH2', 'IY2']"
+    alternatives="['T', 'D', 'T AH0', 'D AH0', 'IH0 D', 'IH1 D', 'IH2 D', 'EH2 D', 'AH0 D']"
+    # alternatives="['T', 'D', 'IH0 D', 'IH1 D', 'IH2 D', 'EH2 D', 'AH0 D']"
+    # alternatives="['T', 'D']"
+    call_phoneme_contrast(filename='ed_acceptEED.wav', text='accepted',  word_id=0, target="IH0 D", alternatives=alternatives)
