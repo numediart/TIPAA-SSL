3ed58253ab759226ea0fe03d1970859e3103d96a
noetits <noe.tits@gmail.com>
2021-06-17 18:56:03 +0200

    update unit tests with new syntax with the separation of audio file processing and speech processing

--- test_modules.py
+++ test_modules.py
@@ -5,37 +5,75 @@ from module_performance import *
 from label_data_processing import *
 
 def test_wordStress():
-    r=wordStress()
-    p=set_params(sentenceID=111, waveFileAddress='audio_recordings/SS_1_i_would_love_to_go_to_ireland.wav', module='wordStress')
-    r=wordStress(p)
+    p=set_params(sentenceID=111, module='wordStress')
+    status_audio, rID=prepare_audio_file('audio_recordings/WS_111_toothpaste.wav')
+    p['rand_fileName']=rID
+    wordStress(p)
+
+    p=set_params(sentenceID=1, module='wordStress')
+    status_audio, rID=prepare_audio_file('audio_recordings/SS_1_i_would_love_to_go_to_ireland.wav')
+    p['rand_fileName']=rID
+    wordStress(p)
 
 def test_sentenceStress():
-    sentenceStress()
-    sentenceStress(p=set_params(sentenceID=1, waveFileAddress='audio_recordings/WS_111_toothpaste.wav', module='sentenceStress'))
+    a,textDict=get_sentenceStress_annotation()
+
+    p=set_params(sentenceID=1, module='sentenceStress')
+    status_audio, rID=prepare_audio_file('audio_recordings/SS_1_i_would_love_to_go_to_ireland.wav')
+    p['rand_fileName']=rID
+    p=make_all_phones_annotation_files(p,remove_special_characters(textDict[p['sentenceID']]))
+    sentenceStress(p)
+
+    p=set_params(sentenceID=1, module='sentenceStress')
+    status_audio, rID=prepare_audio_file('audio_recordings/WS_111_toothpaste.wav')
+    p['rand_fileName']=rID
+    p=make_all_phones_annotation_files(p,remove_special_characters(textDict[p['sentenceID']]))
+    sentenceStress(p)
 
 def test_audio_formats():
-    sentenceStress(p=set_params(sentenceID=1, waveFileAddress='audio_recordings/SS_1_i_would_love_to_go_to_ireland.caf', module='sentenceStress'))
-    sentenceStress(p=set_params(sentenceID=1, waveFileAddress='audio_recordings/SS_1_i_would_love_to_go_to_ireland.m4a', module='sentenceStress'))
-    sentenceStress(p=set_params(sentenceID=1, waveFileAddress='audio_recordings/SS_1_i_would_love_to_go_to_ireland_stereo.m4a', module='sentenceStress'))
+    prepare_audio_file('audio_recordings/SS_1_i_would_love_to_go_to_ireland.caf')
+    prepare_audio_file('audio_recordings/SS_1_i_would_love_to_go_to_ireland.m4a')
+    prepare_audio_file('audio_recordings/SS_1_i_would_love_to_go_to_ireland_stereo.m4a')
 
-# def test_iContrast():
-#     iContrast()
 
 def test_pContrast():
-    phonemeContrast()
+    p=set_params(sentenceID=111, waveFileAddress='audio_recordings/Low_WAV.wav', module="oContrast")
+    # prepare_audio_file(p)
+    status_audio, rID=prepare_audio_file('audio_recordings/Low_WAV.wav')
+    p['rand_fileName']=rID
+    phonemeContrast(p)
 
-def test_chunking():
-    chunking()
 
 def test_edAnalysis():
-    edAnalysis()
+    p=set_params(sentenceID=1, waveFileAddress='audio_recordings/ed_accepted.wav', module="edAnalysis")
+    # prepare_audio_file(p)
+    status_audio, rID=prepare_audio_file('audio_recordings/ed_accepted.wav')
+    p['rand_fileName']=rID
+    edAnalysis(p)
 
 def test_vowel_stresses():
-    vowel_stresses_from_phonetics_audio()
-    vowel_stresses_from_text_audio()
+    p=set_params()
+    # prepare_audio_file(p)
+    status_audio, rID=prepare_audio_file('audio_recordings/SS_1_i_would_love_to_go_to_ireland.wav')
+    p['rand_fileName']=rID
+    vowel_stresses_from_phonetics_audio(p=p)
+
+    status_audio, rID=prepare_audio_file('audio_recordings/SS_1_i_would_love_to_go_to_ireland.wav')
+    p['rand_fileName']=rID
+    sentenceStress_from_phonetics_audio(p=p)
+
+    status_audio, rID=prepare_audio_file('audio_recordings/SS_1_i_would_love_to_go_to_ireland.wav')
+    p['rand_fileName']=rID
+    wordStress_from_phonetics_audio(p=p)
+
+
 
 def test_phonemeContrast():
-    phonemeContrast_from_text_audio()
+    p=set_params(waveFileAddress='audio_recordings/turned_around.mp3')
+    # prepare_audio_file(p)
+    status_audio, rID=prepare_audio_file('audio_recordings/turned_around.mp3')
+    p['rand_fileName']=rID
+    phonemeContrast_from_phonetics_audio(p=p)
 
 
 def test_text_processing():
