ad8fce457b256a78a0580b8aa9e98916dbf55d40
noetits <noe.tits@gmail.com>
2021-09-30 17:03:45 +0200

    removed test of old functions

--- test_modules.py
+++ test_modules.py
@@ -3,29 +3,6 @@ from speech_tech import *
 from text_processing import word_stress_from_text
 from module_performance import *
 from label_data_processing import *
-
-def test_wordStress():
-    d=get_data()
-
-    status_audio, rID=prepare_audio_file('audio_recordings/WS_111_toothpaste.wav')
-    p=make_all_phones_annotation_files(rID,remove_special_characters(d[d.analysisId==111].text.values[0]))
-    wordStress(rID,rID)
-
-    status_audio, rID=prepare_audio_file('audio_recordings/SS_1_i_would_love_to_go_to_ireland.wav')
-    p=make_all_phones_annotation_files(rID,remove_special_characters(d[d.analysisId==1].text.values[0]))
-    wordStress(rID,rID)
-
-def test_sentenceStress():
-    a,textDict=get_sentenceStress_annotation()
-
-    status_audio, rID=prepare_audio_file('audio_recordings/SS_1_i_would_love_to_go_to_ireland.wav')
-    p=make_all_phones_annotation_files(rID,remove_special_characters(textDict[1]))
-    sentenceStress(rID,rID)
-
-    status_audio, rID=prepare_audio_file('audio_recordings/WS_111_toothpaste.wav')
-    p=make_all_phones_annotation_files(rID,remove_special_characters(textDict[1]))
-    sentenceStress(rID,rID)
-
 def test_audio_formats():
     prepare_audio_file('audio_recordings/SS_1_i_would_love_to_go_to_ireland.caf')
     prepare_audio_file('audio_recordings/SS_1_i_would_love_to_go_to_ireland.m4a')
@@ -92,6 +69,31 @@ def test_label_data_processing():
     make_all_phones_annotation_files_from_phonetics(rID)
     make_pContrast_annotation_files_from_phonetics(rID)
     make_pContrast_annotation_files(rID)
+
+# tests of old functions
+if False:
+    def test_wordStress():
+        d=get_data()
+
+        status_audio, rID=prepare_audio_file('audio_recordings/WS_111_toothpaste.wav')
+        p=make_all_phones_annotation_files(rID,remove_special_characters(d[d.analysisId==111].text.values[0]))
+        wordStress(rID,rID)
+
+        status_audio, rID=prepare_audio_file('audio_recordings/SS_1_i_would_love_to_go_to_ireland.wav')
+        p=make_all_phones_annotation_files(rID,remove_special_characters(d[d.analysisId==1].text.values[0]))
+        wordStress(rID,rID)
+
+    def test_sentenceStress():
+        a,textDict=get_sentenceStress_annotation()
+
+        status_audio, rID=prepare_audio_file('audio_recordings/SS_1_i_would_love_to_go_to_ireland.wav')
+        p=make_all_phones_annotation_files(rID,remove_special_characters(textDict[1]))
+        sentenceStress(rID,rID)
+
+        status_audio, rID=prepare_audio_file('audio_recordings/WS_111_toothpaste.wav')
+        p=make_all_phones_annotation_files(rID,remove_special_characters(textDict[1]))
+        sentenceStress(rID,rID)
+
 
 
 if __name__ == '__main__':
