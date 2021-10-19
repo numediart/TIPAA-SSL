518ec6e453620748e5b4e9b7fb4b54818c088628
noetits <noe.tits@gmail.com>
2021-10-19 10:05:14 +0200

    added tests of the API to do with Filipe's DB

--- test_api.py
+++ test_api.py
@@ -3,7 +3,7 @@ from dummy_client import *
 import os
 os.environ['FLOWSPEECH_KEY']="ThisIsTheFlowchaseSP-APIKey:MeaningOfLife=42"
 import ast
-
+from label_data_processing import get_data_new_content, get_data
 
 def test_api():
     # c=app.test_client()
@@ -38,5 +38,115 @@ def test_api():
         # assert eval(call_phoneme_contrast(client=c).data)['status']=='success'
         # assert eval(call_module(module='wordStress', filename='WS_111_toothpaste.wav', sentenceID=111, client=c).data)['status']=='success'
 
+
+def pContrast_for_row(r, url = 'http://localhost:8000/phonemeContrast', client=requests):
+    res=send_audio_base64(path='data/scaleway-audio-files/'+r.audio_file_url, client=client)
+    assert res.status_code == 200
+    res=ast.literal_eval(res.data.decode('utf-8'))
+    assert res['status']=='success'
+    rID=res['rID']
+    print(res)
+
+    res = client.post(url, data={"phonetics":r.cmu_phonetics, 'rID':rID, 
+                                'word_idx':str(ast.literal_eval(r.target_word_indexes)[0]), 
+                                'syl_idx':str(ast.literal_eval(r.target_syllable_indexes)[0]), 
+                                'alternatives':' '.join(ast.literal_eval(r.alternative_phonemes)), 
+                                'target':r.target_phoneme})
+    return res
+
+
+
+def test_GE_linguistic_data_content(base_url = 'http://localhost:8000'):
+    df=pd.read_csv('data/exercise_data_export.csv')
+
+    # df.target_phoneme.dropna().unique()
+    # those who don't have NaN in target
+    df_pContrast=df.loc[df.target_phoneme.dropna().index]
+    df_pContrast.index=range(len(df_pContrast))
+
+    url=base_url+"/phonemeContrast"
+    # client=app.test_client()
+    with app.test_client() as client:
+        results=[]
+        for i,r in df_pContrast.iterrows():
+            res=pContrast_for_row(r, url = url, client=client)
+
+            if '''"phonetic_detection":''' in res.data.decode('utf-8'):
+                result=ast.literal_eval(res.data.decode('utf-8'))
+            else:
+                result={}
+                result['status']=res.data.decode('utf-8')
+                result['phonetic_detection']=''
+                result['gibberish_truth']=''
+                result['gibberish_detected']=''
+            results.append(result)
+        
+        results=pd.DataFrame.from_records(results)
+
+        results['target_phoneme']=df_pContrast['target_phoneme']
+        results['alternative_phonemes']=df_pContrast['alternative_phonemes']
+        results['audio_file_url']=df_pContrast['audio_file_url']
+        
+        results[results.status!='success']
+        results[results.status!='success'].index.tolist()
+
+        ratio=len(results[results.gibberish_truth==results.gibberish_detected])/len(results)
+        results[results.gibberish_truth!=results.gibberish_detected]
+        results[results.gibberish_truth!=results.gibberish_detected][results.audio_file_url.str.contains('F1')]
+        results[results.gibberish_truth!=results.gibberish_detected][results.audio_file_url.str.contains('F2')]
+        results[results.gibberish_truth!=results.gibberish_detected][results.audio_file_url.str.contains('M1')]
+        results[results.gibberish_truth!=results.gibberish_detected][results.audio_file_url.str.contains('M_')]
+        results[results.gibberish_truth!=results.gibberish_detected][results.audio_file_url.str.contains('F_')]
+
+        row=df_pContrast.iloc[results[results.status!='success'].index.tolist()].iloc[2]
+        pContrast_for_row(row, url = url, client=client)
+
+        # select phrases for which the target is not the first alternative (which indicates it is a wrong set of alternatives)
+        incorrect_alternatives_phrase_ids=df_pContrast[(df_pContrast.target_phoneme!=df_pContrast['alternative_phonemes'].apply(lambda r:ast.literal_eval(r)[0]))
+                        &(~df_pContrast.target_phoneme.isin(['D','T','IH0_D']))].fk_phrase_id.unique()
+        incorrect_alternatives_phrases=df_pContrast[df_pContrast.fk_phrase_id.isin(incorrect_alternatives_phrase_ids)].drop_duplicates(subset='fk_phrase_id', keep="first")
+        
+        for p in incorrect_alternatives_phrases.target_phoneme.unique():
+            print(p, incorrect_alternatives_phrases[incorrect_alternatives_phrases.target_phoneme==p].fk_phrase_id.tolist())
+        
+        # select phrases for which there are more than stress variation in the alternatives, which should not happen
+        incorrect_alternatives_phrase_ids2=df_pContrast[(df_pContrast.apply(lambda r:len(set([el[-1] for el in ast.literal_eval(r['alternative_phonemes'])])), axis=1)>1)&(~df_pContrast.target_phoneme.isin(['D','T','IH0_D']))].fk_phrase_id.unique()
+        incorrect_alternatives_phrases2=df_pContrast[df_pContrast.fk_phrase_id.isin(incorrect_alternatives_phrase_ids2)].drop_duplicates(subset='fk_phrase_id', keep="first")
+        
+        for p in incorrect_alternatives_phrases2.target_phoneme.unique():
+            print(p, incorrect_alternatives_phrases2[incorrect_alternatives_phrases2.target_phoneme==p].fk_phrase_id.tolist())
+
+def test_particular_cases(base_url = 'http://localhost:8000'):
+    
+    df=pd.read_csv('data/exercise_data_export.csv')
+
+    # df.target_phoneme.dropna().unique()
+    # those who don't have NaN in target
+    df_pContrast=df.loc[df.target_phoneme.dropna().index]
+    
+    url=base_url+"/phonemeContrast"
+    # client=app.test_client()
+
+    # indices selected of historical errors from above function that are therefore interesting test cases 
+    selected_idxs=[968, 1123, 1524] 
+    with app.test_client() as client:
+        results=[]
+        for idx in selected_idxs:
+            row=df.iloc[idx]
+            res=pContrast_for_row(row, url = url, client=client)
+            if '''"phonetic_detection":''' in res.data.decode('utf-8'):
+                result=ast.literal_eval(res.data.decode('utf-8'))
+            else:
+                result={}
+                result['status']=res.data.decode('utf-8')
+                result['phonetic_detection']=''
+                result['gibberish_truth']=''
+                result['gibberish_detected']=''
+            results.append(result)
+        
+        results=pd.DataFrame.from_records(results)
+
 if __name__ == '__main__':
-    test_api()
+    # test_api()
+    # test_GE_linguistic_data_content()
+    test_particular_cases()
