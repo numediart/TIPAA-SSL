59a3b8bc565edacb1995762a5c75b5843fe1ee5d
noetits <noe.tits@gmail.com>
2021-11-03 12:11:51 +0100

    -generate alternatives on runtime from target -post-corrections for AA/AO and D/T to be more accepting
    -experimental "final_phoneme" endpoint putting pretermination in target to have an alternative without the phoneme

--- flask_server.py
+++ flask_server.py
@@ -309,6 +309,41 @@ def send_audio():
 
 
 
+# set alternatives from targets
+def set_alternatives_from_target(target):
+    ED_targets=['_T','_D','_IH0_D']
+
+    # target_to_alternatives={
+    #     'IH':['IH','IY','AA','AO','AW','AY','ER','OY'], # from   https://docs.google.com/spreadsheets/d/1tzb7ZKQOifCHXh-Aoz4PdAKPlIquThzk80EvXW1UxLw/edit#gid=0
+    #     'IY':['IY','IH','AA','AE','AH','AO','AW','AY','EH','ER','OW','OY','UH'],
+    #     'AO':['AO','OW','AW','EH','ER','EY','IH','IY','OY','UH','UW'],
+    #     'AA':['AA','OW','AW','EH','ER','EY','IH','IY','OY','UH','UW'],
+    #     'OW':['OW','AA','AO','AE','AY','ER','EY','IH','IY','OY','UH'],
+    #     "IH0_D":['T', 'D', 'IH0_D'],
+    #     "D":['T', 'D', 'IH0_D'],
+    #     "T":['T', 'D', 'IH0_D']
+    # }
+
+    # New version based on a_test_GE_linguistic_data_content results in test_api
+    target_to_alternatives={
+        'IH':['IH','IY','AO','AW','AY','ER','OY'],
+        'IY':['IY','IH','AA','AE','AH','AO','AW','AY','EH','ER','OW','OY','UH'],
+        'AO':['AO','AA','OW','AW','ER','IY','OY'],
+        'AA':['AA','AO','OW','AW','ER'],#,'IY','OY'
+        'OW':['OW','AA','AO'],#,'IY','OY'
+        "IH0_D":['T', 'D', 'IH0_D'],
+        "D":['T', 'D', 'IH0_D'],
+        "T":['T', 'D', 'IH0_D']
+    }
+
+    alternatives=float('nan')
+
+    if target[-1] in str([0,1,2]):
+        alternatives=' '.join([el+target[-1] for el in target_to_alternatives[target[:-1]]])
+    elif '_'+target in ED_targets:
+        alternatives=' '.join(target_to_alternatives[target])
+    return alternatives
+
 
 responseSchema=Schema.from_dict(
     {
@@ -378,7 +413,8 @@ properties=["phonetics","rID","word_idx","target","syl_idx","alternatives"]
 @app.route('/phonemeContrast', methods=['POST'])
 def phoneme_contrast_api():
     content = request.form
-    properties=["phonetics","rID","word_idx","target","syl_idx","alternatives"]
+    # properties=["phonetics","rID","word_idx","target","syl_idx","alternatives"]
+    properties=["phonetics","rID","word_idx","target","syl_idx"]
     for prop in properties:
         err=access_property_error(content, prop)
         if err: return Response(err,status=400,mimetype="application/json")
@@ -396,14 +432,15 @@ def phoneme_contrast_api():
         err="error: "+not_p+" is not a phoneme"
         return Response(err,status=400,mimetype="application/json")
 
-
     word_idx=content['word_idx']
     syl_idx=content['syl_idx']
     rID=content['rID']
     target=content['target']
-    alternatives=content['alternatives']
-    print(alternatives)
+    # alternatives=content['alternatives']
+    
 
+    alternatives=set_alternatives_from_target(target)
+    print(alternatives)
     word_idx=int(word_idx)
     syl_idx=int(syl_idx)
 
@@ -452,10 +489,145 @@ def phoneme_contrast_api():
         gs_t.append('_'.join(g_t))
         gs_d.append('_'.join(g_d))
     
+    # A posteriori correction for phonemes that are too close to be considered different
+    if 'AA' in target or 'AO' in target:
+        if 'AA' in phonetic_detection[target_idx] or 'AO' in phonetic_detection[target_idx]:
+            phonetic_detection[target_idx]=target
+            gs_d[target_idx]=gs_t[target_idx]
+    if target=='D' or target=='T':
+        if phonetic_detection[target_idx]=='D' or phonetic_detection[target_idx]=='T':
+            phonetic_detection[target_idx]=target
+            gs_d[target_idx]=gs_t[target_idx]
+    
     d={'status':status, 'phonetic_detection':phonetic_detection[target_idx], 'gibberish_truth':gs_t[target_idx], 'gibberish_detected':gs_d[target_idx]}
     response=json.dumps(d)
     return response
 
+
+
+responseSchema=Schema.from_dict(
+    {"status": fields.Str(), 
+    "phonetic_detection": fields.Str(), 
+    "gibberish_truth": fields.Str(),
+    "gibberish_detected":fields.Str()},
+    name="phonemeContrast_response"
+)
+properties=["phonetics","rID","word_idx","target","syl_idx","alternatives"]
+@doc(description='Phoneme contrast', tags=['phonemeContrast'])
+@use_kwargs(properties_to_args(properties), location=('form'))
+@marshal_with(responseSchema, code=200)  # marshalling
+@app.route('/final_phoneme', methods=['POST'])
+def final_phoneme_api():
+    final_phoneme=True
+
+    content = request.form
+    # properties=["phonetics","rID","word_idx","target","syl_idx","alternatives"]
+    properties=["phonetics","rID","word_idx","target","syl_idx"]
+    for prop in properties:
+        err=access_property_error(content, prop)
+        if err: return Response(err,status=400,mimetype="application/json")
+    
+    # print(request.__dict__)
+    print(content['phonetics'])
+    print(content['word_idx'])
+    print(content['rID'])
+    phonetics=content['phonetics']
+
+    split_phonetics=[[s.split('_') for s in w.split('|')] for w in phonetics.split(' ')]
+    merged_phonetics=[merge_list(word) for word in split_phonetics]    
+    not_p=check_phonemes(merge_list(merged_phonetics))
+    if not_p is not None: 
+        err="error: "+not_p+" is not a phoneme"
+        return Response(err,status=400,mimetype="application/json")
+
+    word_idx=content['word_idx']
+    syl_idx=content['syl_idx']
+    rID=content['rID']
+    target=content['target']
+    # alternatives=content['alternatives']
+    
+    alternatives=set_alternatives_from_target(target)
+    print(alternatives)
+    word_idx=int(word_idx)
+    syl_idx=int(syl_idx)
+
+    # split_phonetics=[[s.split('_') for s in w.split('|')] for w in phonetics.split(' ')]
+
+    if word_idx>=len(phonetics.split(' ')):
+        return Response("error: word_idx >= number of words",status=400,mimetype="application/json")
+    
+    # extract the syllables which contain the target
+    syls_with_target=[syl for syl in phonetics.split(' ')[word_idx].split('|') if target in syl]
+    idx_syls_with_target=[i for i,syl in enumerate(phonetics.split(' ')[word_idx].split('|')) if target in syl]
+
+    if syl_idx not in idx_syls_with_target:
+        return Response("error: the syllable corresponding to syl_idx does not contain the target.",status=400,mimetype="application/json")
+    target_idx=idx_syls_with_target.index(syl_idx)
+    if final_phoneme:
+        # to discriminate between with/without termination, 
+        # we put the pretermination in the target and put it at the start of each alternative + add it alone
+        target_syl=[syl for syl in phonetics.split(' ')[word_idx].split('|')][syl_idx]
+        idx_target_in_syl=target_syl.replace(target,'TAR').split('_').index('TAR')
+        pretermination=target_syl.split('_')[idx_target_in_syl-1]
+        alternatives=' '.join([pretermination+'_'+el for el in alternatives.split(' ')]+[pretermination])
+        target=pretermination+'_'+target
+
+    status,result=phonemeContrast_from_formatted_phonetics_audio(rID, phonetics, word_idx, target, alternatives)
+    print('status:',status)
+    print('result:',result)
+    if not isinstance(result, list):
+        result=result.tolist()
+        print('result:',result)
+    if result!=[]:  
+        # phonetic_transcript=result[-1]
+        phonetic_detection=result[0][result[0].iloc[:,2].str.contains('_')].detected_transcription.tolist()
+    else:
+        # phonetic_detection=result
+        return Response(status,status=200,mimetype="application/json")
+
+    if phonetic_detection==[]: return Response("success: phonetic detection is empty",status=200,mimetype="application/json")
+    
+    if final_phoneme:
+        phonetic_detection=[el.replace(pretermination+'_','') if el!=pretermination else 'not pronounced' for el in phonetic_detection]
+        # remove the pretermination that was added at the start of target
+        target='_'.join(target.split('_')[1:])
+
+    syls_detection=[]
+    for i,syl in enumerate(syls_with_target):
+        try:
+            phonetic_detection[i]
+        except:
+            return Response("error: index out of bounds in phonetic detection",status=500,mimetype="application/json")
+        syls_detection.append(syl.replace(target, phonetic_detection[i]))
+
+    gs_t=[]
+    gs_d=[]
+    for syl_t,syl_d in zip(syls_with_target,syls_detection):
+        g_t=[cmu_to_gibberish[el] if not el[-1] in str([0,1,2]) else cmu_to_gibberish[el[:-1]] for el in syl_t.split('_')]
+
+        g_d=[]
+        for el in  syl_d.split('_'):
+            if el=='not pronounced': g_d.append(el)
+            elif not el[-1] in str([0,1,2]): g_d.append(cmu_to_gibberish[el])
+            else: g_d.append(cmu_to_gibberish[el[:-1]])
+
+        # g_d=[cmu_to_gibberish[el] if not el[-1] in str([0,1,2]) else cmu_to_gibberish[el[:-1]] for el in syl_d.split('_')]
+        gs_t.append('_'.join(g_t))
+        gs_d.append('_'.join(g_d))
+    
+    # A posteriori correction for phonemes that are too close to be considered different
+    if 'AA' in target or 'AO' in target:
+        if 'AA' in phonetic_detection[target_idx] or 'AO' in phonetic_detection[target_idx]:
+            phonetic_detection[target_idx]=target
+            gs_d[target_idx]=gs_t[target_idx]
+    if target=='D' or target=='T':
+        if phonetic_detection[target_idx]=='D' or phonetic_detection[target_idx]=='T':
+            phonetic_detection[target_idx]=target
+            gs_d[target_idx]=gs_t[target_idx]
+    
+    d={'status':status, 'phonetic_detection':phonetic_detection[target_idx], 'gibberish_truth':gs_t[target_idx], 'gibberish_detected':gs_d[target_idx]}
+    response=json.dumps(d)
+    return response
 
 
 record={'text':fields.Str(),
@@ -476,7 +648,7 @@ responseSchema=Schema.from_dict(record, name="prefill response")
 @use_kwargs({'phrase':fields.String(required=True, description="Text sentence to be processed. It can contain special characters etc.")}, location=('form'))
 @marshal_with(responseSchema, code=200)  # marshalling
 @app.route('/prefill_from_phrase', methods=['POST'])
-@debug_only 
+@debug_only
 def prefill_from_phrase():
     content = request.form
     
@@ -503,48 +675,6 @@ docs.register(send_audio)
 docs.register(prefill_from_phrase)
 docs.register(phoneme_contrast_api)
 docs.register(module_api)
-
-# deprecated functions
-if False:
-    @app.route('/flowspeech/<module>', methods=['POST'])
-    def module_api(module):
-        content = request.form
-        # import pdb;pdb.set_trace()
-        # print(request.__dict__)
-        print(content)
-        print(content['sentenceID'])
-        print(content['filename'])
-        # return jsonify({"uuid":uuid})
-        filename=content['filename']
-        sentenceID=content['sentenceID']
-        p=set_params(sentenceID=int(sentenceID), waveFileAddress=upload_path+filename, module=module)
-        try:
-            method_to_call = getattr(speech_tech, module)
-        except:
-            print('no such module exists')
-            response="-1"
-            return response
-        res=method_to_call(p)
-        print(res)
-        response=json.dumps(res)
-        return response
-
-    
-    upload_path="./upload_files/"
-    @app.route('/', methods=['POST'])
-    def upload_file():
-        try:
-            uploaded_file = request.files['file']
-        except:
-            return "error: could not access request.files['file'] "
-        if uploaded_file.filename != '':
-            try:
-                uploaded_file.save(upload_path+uploaded_file.filename)
-            except:
-                return "error: could not save uploaded file"
-        else:
-            return "error: filename is empty"
-        return "success"
 
 if __name__ == '__main__':
     run_app()
