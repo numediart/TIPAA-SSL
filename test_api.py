72d487858e0b624ea82a6276033f116a31a332a8
noetits <noe.tits@gmail.com>
2021-12-24 12:04:50 +0100

    get phoneme contrast results from servers and local api + comparisons

--- test_api.py
+++ test_api.py
@@ -37,9 +37,12 @@ def test_api(c=app.test_client()):
         # assert eval(call_module(module='wordStress', filename='WS_111_toothpaste.wav', sentenceID=111, client=c).data)['status']=='success'
 
 
-def pContrast_for_row(r, url = 'http://localhost:8000/phonemeContrast', client=requests):
-    res=send_audio_base64(path='data/scaleway-audio-files/'+r.audio_file_url, client=client)
+def pContrast_for_row(r, base_url = 'http://localhost:8000', endpoint='/phonemeContrast', client=requests):
+    url=base_url+endpoint
+    res=send_audio_base64(path='data/scaleway-audio-files/'+r.audio_file_url, base_url = base_url, client=client)
     assert res.status_code == 200
+    # This is for compatibility between requests module and flask's test_client
+    if client==requests: res.data=res._content
     res=ast.literal_eval(res.data.decode('utf-8'))
     assert res['status']=='success'
     rID=res['rID']
@@ -50,11 +53,13 @@ def pContrast_for_row(r, url = 'http://localhost:8000/phonemeContrast', client=r
                                 'syl_idx':str(ast.literal_eval(r.target_syllable_indexes)[0]), 
                                 'alternatives':' '.join(ast.literal_eval(r.alternative_phonemes)), 
                                 'target':r.target_phoneme})
-
+    
+    # This is for compatibility between requests module and flask's test_client
+    if client==requests: res.data=res._content
     return res
 
 
-def test_GE_linguistic_data_content(url = 'http://localhost:8000/phonemeContrast'):
+def test_GE_linguistic_data_content(base_url = 'http://localhost:8000', endpoint='/phonemeContrast', client=app.test_client()):
     df=pd.read_csv('data/exercise_data_export.csv')
 
     # df.target_phoneme.dropna().unique()
@@ -62,35 +67,36 @@ def test_GE_linguistic_data_content(url = 'http://localhost:8000/phonemeContrast
     df_pContrast=df.loc[df.target_phoneme.dropna().index]
 
     # client=app.test_client()
-    with app.test_client() as client:
-        results=[]
-        for i,r in df_pContrast.iterrows():
-            res=pContrast_for_row(r, url = url, client=client)
+    # with app.test_client() as client:
+
+    results=[]
+    # for i,r in df_pContrast.iterrows():
+    #     res=pContrast_for_row(r, base_url = base_url, endpoint=endpoint, client=client)
 
     df_pContrast.index=range(len(df_pContrast))
 
     # df_ed.index=range(len(df_ed))
     # df_v.index=range(len(df_v))
 
-    def get_results(df_pContrast, url = 'http://localhost:8000/phonemeContrast'):
+    def get_results(df_pContrast, base_url = 'http://localhost:8000', endpoint='/phonemeContrast', client=client):
         # client=app.test_client()
-        with app.test_client() as client:
-            results=[]
-            for i,r in df_pContrast.iterrows():
-                res=pContrast_for_row(r, url = url, client=client)
-                if '''"phonetic_detection":''' in res.data.decode('utf-8'):
-                    result=ast.literal_eval(res.data.decode('utf-8'))
-                else:
-                    result={}
-                    result['status']=res.data.decode('utf-8')
-                    result['phonetic_detection']=''
-                    result['gibberish_truth']=''
-                    result['gibberish_detected']=''
-                results.append(result)
-            
-            results=pd.DataFrame.from_records(results)
-            # row=df_pContrast.iloc[results[results.status!='success'].index.tolist()].iloc[2]
-            # pContrast_for_row(row, url = url, client=client)
+        # with app.test_client() as client:
+        results=[]
+        for i,r in df_pContrast.iterrows():
+            res=pContrast_for_row(r, base_url = base_url, endpoint=endpoint, client=client)
+            if '''"phonetic_detection":''' in res.data.decode('utf-8'):
+                result=ast.literal_eval(res.data.decode('utf-8'))
+            else:
+                result={}
+                result['status']=res.data.decode('utf-8')
+                result['phonetic_detection']=''
+                result['gibberish_truth']=''
+                result['gibberish_detected']=''
+            results.append(result)
+        
+        results=pd.DataFrame.from_records(results)
+        # row=df_pContrast.iloc[results[results.status!='success'].index.tolist()].iloc[2]
+        # pContrast_for_row(row, url = url, client=client)
         results['target_phoneme']=df_pContrast['target_phoneme']
         results['alternative_phonemes']=df_pContrast['alternative_phonemes']
         results['audio_file_url']=df_pContrast['audio_file_url']
@@ -111,7 +117,7 @@ def test_GE_linguistic_data_content(url = 'http://localhost:8000/phonemeContrast
     df_pContrast[-1:].cmu_phonetics
     
     # results=get_results(df_pContrast, url=base_url+"/phonemeContrast")
-    results=get_results(df_pContrast, url=url)
+    results=get_results(df_pContrast, base_url = base_url, endpoint=endpoint)
     # df_ed=df_ed[-2:]
     # results_ed=get_results(df_ed, url=base_url+"/final_phoneme")
     # results_v=get_results(df_v, url=base_url+"/phonemeContrast")
@@ -176,7 +182,7 @@ def test_GE_linguistic_data_content(url = 'http://localhost:8000/phonemeContrast
 
     return results
 
-def test_particular_cases(url = 'http://localhost:8000/phonemeContrast'):
+def test_particular_cases(base_url = 'http://localhost:8000', endpoint='/phonemeContrast', client=app.test_client()):
     
     df=pd.read_csv('data/exercise_data_export.csv')
 
@@ -189,58 +195,121 @@ def test_particular_cases(url = 'http://localhost:8000/phonemeContrast'):
     # indices selected of historical errors from above function that are therefore interesting test cases 
     # selected_idxs=[1524] 
     selected_idxs=[968, 1123, 1524] 
-    with app.test_client() as client:
-        results=[]
-        for idx in selected_idxs:
-            row=df.iloc[idx]
-            res=pContrast_for_row(row, url = url, client=client)
-            if '''"phonetic_detection":''' in res.data.decode('utf-8'):
-                result=ast.literal_eval(res.data.decode('utf-8'))
-            else:
-                result={}
-                result['status']=res.data.decode('utf-8')
-                result['phonetic_detection']=''
-                result['gibberish_truth']=''
-                result['gibberish_detected']=''
-            results.append(result)
-        results=pd.DataFrame.from_records(results)
-        return results
+    # with app.test_client() as client:
+
+    results=[]
+    for idx in selected_idxs:
+        row=df.iloc[idx]
+        res=pContrast_for_row(row, base_url = base_url, endpoint=endpoint, client=client)
+        if '''"phonetic_detection":''' in res.data.decode('utf-8'):
+            result=ast.literal_eval(res.data.decode('utf-8'))
+        else:
+            result={}
+            result['status']=res.data.decode('utf-8')
+            result['phonetic_detection']=''
+            result['gibberish_truth']=''
+            result['gibberish_detected']=''
+        results.append(result)
+    results=pd.DataFrame.from_records(results)
+    return results
+
+
+def get_server_and_local_results():
+    res={}
+
+    res['rpC_dev']=test_GE_linguistic_data_content('https://dev-speech-processing.flowchase.app', endpoint='/phonemeContrast', client=requests)
+    res['r_ter_dev']=test_GE_linguistic_data_content('https://dev-speech-processing.flowchase.app', endpoint='/termination_contrast', client=requests)
+
+    res['rpC_prod']=test_GE_linguistic_data_content('https://speech-processing.flowchase.app', endpoint='/phonemeContrast', client=requests)
+    res['r_ter_prod']=test_GE_linguistic_data_content('https://speech-processing.flowchase.app', endpoint='/termination_contrast', client=requests)
+
+    res['rpC_local']=test_GE_linguistic_data_content('http://localhost:8000', endpoint='/phonemeContrast')
+    res['r_ter_local']=test_GE_linguistic_data_content('http://localhost:8000', endpoint='/termination_contrast')
+
+    for k in res: res[k].to_csv('performance_results/'+k+'.csv')
+
+
+    
 
 
 def result_analysis():
     
-    rpC=test_GE_linguistic_data_content('http://localhost:8000/phonemeContrast')
-    r_ter=test_GE_linguistic_data_content('http://localhost:8000/termination_contrast')
+    # rpC=test_GE_linguistic_data_content('http://localhost:8000/phonemeContrast')
+    # r_ter=test_GE_linguistic_data_content('http://localhost:8000/termination_contrast')
 
     
-    rpC.to_csv('results/test_api_phonemeContrast.csv')
-    r_ter.to_csv('results/test_api_termination_contrast.csv')
-
-    # rpC=pd.read_csv('results/test_api_phonemeContrast.csv')
-    # r_ter=pd.read_csv('results/test_api_termination_contrast.csv')
-
-    len(rpC[~(r_ter==rpC).gibberish_detected])
-
-    rpC[~(r_ter==rpC).gibberish_detected][rpC.target_phoneme=='D']
-    r_ter[~(r_ter==rpC).gibberish_detected][r_ter.target_phoneme=='D']
-    rpC[~(r_ter==rpC).gibberish_detected][rpC.target_phoneme=='T']
-    r_ter[~(r_ter==rpC).gibberish_detected][r_ter.target_phoneme=='T']
+    # rpC.to_csv('performance_results/test_api_phonemeContrast.csv')
+    # r_ter.to_csv('performance_results/test_api_termination_contrast.csv')
 
     
-    rpC[~(r_ter==rpC).gibberish_detected][rpC.target_phoneme.str.contains('AO')]
-    r_ter[~(r_ter==rpC).gibberish_detected][r_ter.target_phoneme.str.contains('AO')]
-    rpC[~(r_ter==rpC).gibberish_detected][rpC.target_phoneme.str.contains('OW')]
-    r_ter[~(r_ter==rpC).gibberish_detected][r_ter.target_phoneme.str.contains('OW')]
-    rpC[~(r_ter==rpC).gibberish_detected][rpC.target_phoneme.str.contains('IH')]
-    r_ter[~(r_ter==rpC).gibberish_detected][r_ter.target_phoneme.str.contains('IH')]
-    rpC[~(r_ter==rpC).gibberish_detected][rpC.target_phoneme.str.contains('IY')]
-    r_ter[~(r_ter==rpC).gibberish_detected][r_ter.target_phoneme.str.contains('IY')]
+    ks=['rpC_dev', 'r_ter_dev', 'rpC_prod', 'r_ter_prod', 'rpC_local', 'r_ter_local']
+    res={}
+    for k in ks: res[k]=pd.read_csv('performance_results/'+k+'.csv')
+
+    
+
+    # checking nans are either "success: all of the elements were out of vocab" or "success: phonetic detection is empty"
+    res['rpC_dev'][res['rpC_dev'].isnull().any(1)]
+
+    for k in ks: res[k]=pd.read_csv('performance_results/'+k+'.csv').dropna()
+
+    (res['rpC_dev']==res['rpC_prod']).sum()
+    (res['rpC_dev']==res['rpC_local']).sum()
+
+    (res['rpC_dev']==res['r_ter_dev']).sum()
+    (res['r_ter_dev']==res['r_ter_local']).sum()
+
+    (res['rpC_local']==res['r_ter_local']).sum()
+    (res['rpC_local']!=res['r_ter_local']).sum()
+
+    res['rpC_local'][~(res['r_ter_local']==res['rpC_local']).gibberish_detected]#[res['rpC_local'].target_phoneme=='D']
+    res['r_ter_local'][~(res['r_ter_local']==res['rpC_local']).gibberish_detected]#[res['rpC_local'].target_phoneme=='D']
+
+    # rpC=pd.read_csv('performance_results/test_api_phonemeContrast.csv')
+    # r_ter=pd.read_csv('performance_results/test_api_termination_contrast.csv')
+
+    # len(rpC[~(r_ter==rpC).gibberish_detected])
+
+    # rpC[~(r_ter==rpC).gibberish_detected][rpC.target_phoneme=='D']
+    # r_ter[~(r_ter==rpC).gibberish_detected][r_ter.target_phoneme=='D']
+    # rpC[~(r_ter==rpC).gibberish_detected][rpC.target_phoneme=='T']
+    # r_ter[~(r_ter==rpC).gibberish_detected][r_ter.target_phoneme=='T']
+
+    
+    # rpC[~(r_ter==rpC).gibberish_detected][rpC.target_phoneme.str.contains('AO')]
+    # r_ter[~(r_ter==rpC).gibberish_detected][r_ter.target_phoneme.str.contains('AO')]
+    # rpC[~(r_ter==rpC).gibberish_detected][rpC.target_phoneme.str.contains('OW')]
+    # r_ter[~(r_ter==rpC).gibberish_detected][r_ter.target_phoneme.str.contains('OW')]
+    # rpC[~(r_ter==rpC).gibberish_detected][rpC.target_phoneme.str.contains('IH')]
+    # r_ter[~(r_ter==rpC).gibberish_detected][r_ter.target_phoneme.str.contains('IH')]
+    # rpC[~(r_ter==rpC).gibberish_detected][rpC.target_phoneme.str.contains('IY')]
+    # r_ter[~(r_ter==rpC).gibberish_detected][r_ter.target_phoneme.str.contains('IY')]
 
 
 
 if __name__ == '__main__':
     # test_api()
     # test_GE_linguistic_data_content()
-    rpC=test_GE_linguistic_data_content('http://localhost:8000/phonemeContrast')
-    r_ter=test_GE_linguistic_data_content('http://localhost:8000/termination_contrast')
+    # rpC=test_GE_linguistic_data_content('http://localhost:8000/phonemeContrast')
+
+    # rpC=test_GE_linguistic_data_content('https://dev-speech-processing.flowchase.app/phonemeContrast')
+    # r_ter=test_GE_linguistic_data_content('http://localhost:8000/termination_contrast')
+
+    rpC_dev=test_GE_linguistic_data_content('https://dev-speech-processing.flowchase.app', endpoint='/phonemeContrast', client=requests)
+    r_ter_dev=test_GE_linguistic_data_content('https://dev-speech-processing.flowchase.app', endpoint='/termination_contrast', client=requests)
+
+    rpC_prod=test_GE_linguistic_data_content('https://speech-processing.flowchase.app', endpoint='/phonemeContrast', client=requests)
+    r_ter_prod=test_GE_linguistic_data_content('https://speech-processing.flowchase.app', endpoint='/termination_contrast', client=requests)
+
+    rpC_local=test_GE_linguistic_data_content('http://localhost:8000', endpoint='/phonemeContrast')
+    r_ter_local=test_GE_linguistic_data_content('http://localhost:8000', endpoint='/termination_contrast')
+    
+    test_GE_linguistic_data_content()
+
+    test_particular_cases('https://dev-speech-processing.flowchase.app', endpoint='/phonemeContrast', client=requests)
     test_particular_cases()
+
+
+    base_url='https://dev-speech-processing.flowchase.app'
+    endpoint='/phonemeContrast'
+    url=base_url+endpoint
