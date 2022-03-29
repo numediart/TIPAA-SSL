3a0cb058a94ac1986f710193d8db04a1536476f9
noetits <noe.tits@gmail.com>
2022-03-29 17:25:22 +0300

    a class inheritting from "charsiu" that does forced-alignemnt and phoneme prediction based on wav2vec It is adapted for phoneme contrast and stress detection

---
+++ utils/charsiu_utils.py
@@ -0,0 +1,326 @@
+import os
+import librosa
+import pandas as pd
+import numpy as np
+
+from charsiu.src.Charsiu import charsiu_forced_aligner, charsiu_attention_aligner, charsiu_predictive_aligner
+
+import sys
+import torch
+import numpy as np
+from charsiu.src.utils import seq2duration,forced_align
+from utils.text_processing import remove_stress_annots, phonetics_indexed_df_from_formatted_phonetics, cmu_vowels, unstress
+from utils.audio_processing import getIntonation, getIntensity, normalize
+from collections import Counter
+
+
+class charsiu_phone_forced_aligner(charsiu_forced_aligner):    
+    def align_phones(self, audio, phones):
+        '''
+        Perform forced alignment
+
+        Parameters
+        ----------
+        audio : np.ndarray [shape=(n,)]
+            time series of speech signal
+        text : str
+            The transcription
+
+        Returns
+        -------
+        A tuple of aligned phones in the form (start_time, end_time, phone)
+
+        '''
+        audio = self.charsiu_processor.audio_preprocess(audio,sr=self.sr)
+        audio = torch.Tensor(audio).unsqueeze(0).to(self.device)
+        # phones, words = self.charsiu_processor.get_phones_and_words(text)
+        phone_ids = self.charsiu_processor.get_phone_ids(phones)
+
+        with torch.no_grad():
+            out = self.aligner(audio)
+        
+        hidden_states= out.hidden_states
+        last_hidden_state=hidden_states[-1]
+        logits=out.logits
+
+        cost = torch.softmax(out.logits,dim=-1).detach().cpu().numpy().squeeze()
+
+        pred_ids_audio = torch.argmax(out.logits.squeeze(),dim=-1)
+        pred_ids_audio = pred_ids_audio.detach().cpu().numpy()
+        pred_phones_audio = [self.charsiu_processor.mapping_id2phone(int(i)) for i in pred_ids_audio]
+        # pred_phones = seq2duration(pred_phones,resolution=self.resolution)
+        
+        
+        sil_mask = self._get_sil_mask(cost)
+        nonsil_idx = np.argwhere(sil_mask!=self.charsiu_processor.sil_idx).squeeze()
+
+        if len(nonsil_idx)>0:
+            aligned_phone_ids = forced_align(cost[nonsil_idx,:],phone_ids[1:-1])
+            aligned_phones = [self.charsiu_processor.mapping_id2phone(phone_ids[1:-1][i]) for i in aligned_phone_ids]
+            pred_phones = self._merge_silence(aligned_phones,sil_mask)
+            alignment_phones = seq2duration(pred_phones,resolution=self.resolution)
+
+            
+
+            # for each alignment, go inside the corresponding frames of per frame predictions from audio and do a majority vote
+            timestep=self.resolution
+            most_pred_phones_audio=[]
+            for a in alignment_phones:
+                start=a[0]
+                end=a[1]
+                select=pred_phones_audio[round(start/timestep):round(end/timestep)]
+                c = Counter(select)
+                value, count = c.most_common()[0]
+                most_pred_phones_audio.append(value)
+        else:
+            sil='[SIL]'
+            alignment_phones=[(0, len(pred_ids_audio)*self.resolution, sil)]
+            most_pred_phones_audio=[sil]
+
+        return alignment_phones, most_pred_phones_audio
+    
+    def force_and_predict(self, audio, phones):
+        """phones must be a list of phonemes, e.e.: phones=['EH1', 'N', 'D', 'IH0', 'D']
+        """
+        seq_p=[[p] for p in  remove_stress_annots(phones)]
+
+        # print('seq_p', seq_p)
+        # charsiu.charsiu_processor.get_phone_ids(seq_p)
+        alignment_phones, pred_phones_audio = self.align_phones(audio=audio,phones=seq_p)
+        df_segmented=pd.DataFrame(alignment_phones)
+        df_segmented.columns=['start','end','cmu_phones']
+        df_segmented['pred_phones_audio']=pred_phones_audio
+        return df_segmented
+    
+    def predict_word(self, audio, phonetics, target_word_idx):
+        start_idx=sum([len(p) for p in phonetics][:target_word_idx])
+        end_idx=sum([len(p) for p in phonetics][:target_word_idx+1])
+        # merge lists
+        phonetics=sum(phonetics,[])
+        df_segmented=self.force_and_predict(audio,phonetics)
+        df_segmented=df_segmented[df_segmented.cmu_phones != '[SIL]']
+        df_word=df_segmented[start_idx:end_idx]
+        return df_word
+    
+    def predict_phone(self, audio, phonetics, target_word_idx, target_syllable_idx, target_phones):
+        """phonetics must be formatted phonetics as a string, e.g.: 'EH1_N|D_IH0_D'
+        """
+        split_phonetics=[p.replace('|','_').split('_') for p in phonetics.split(' ')]
+        df_word=self.predict_word(audio, split_phonetics, target_word_idx)
+
+        if len(df_word)>0:
+            word=phonetics.split(' ')[target_word_idx]
+            syllables=[syl.split('_') for syl in word.split('|')]
+            syllable=syllables[target_syllable_idx]
+            
+            syl=remove_stress_annots(syllable)
+
+            # find the phoneme index:
+            p_idx_local=syl.index(unstress(target_phones))
+
+            len_previous_syllables=sum([len(el) for el in syllables[:target_syllable_idx]])
+            p_idx_global=len_previous_syllables+p_idx_local
+            phonetic_detection=df_word.iloc[p_idx_global].pred_phones_audio
+        else:
+            phonetic_detection=float('nan')
+
+        return phonetic_detection
+
+    def compute_stress_score(self, audio, phonetics):
+        """Use textgridData to have the timings of vowels and compute prosody features (intesity, pitch, ...) to compute 
+        a value by vowel representing a stress intensity
+
+        Args:
+            textgridData ([type]): [description]
+            s (np array): audio signal
+            fs (int): frequency of sampling
+        Returns:
+            weighted_score [type]: stress intensity score
+        """
+        
+        # phonetics=sum(phonetics,[])
+        split_phonetics=[p.replace('|','_').split('_') for p in phonetics.split(' ')]
+        split_phonetics=sum(split_phonetics,[])
+
+        textgridData=self.force_and_predict(audio,split_phonetics)
+        textgridData=textgridData[textgridData.cmu_phones != '[SIL]']
+
+        # print(textgridData)
+        
+        a=textgridData
+        a.loc[a.cmu_phones.shift(-1) == a.cmu_phones]
+        # if len(a.loc[a.cmu_phones.shift(-1) == a.cmu_phones])>0:  print(a.loc[a.cmu_phones.shift(-1) == a.cmu_phones]); import pdb;pdb.set_trace()
+
+        # I have to collapse if several consecutive vowels are the same. it can happen when the predictions are not the same.
+        # I thus have to group the timings (first start until last end)
+
+        #  here we delete consecutives but keep first and last, so there is a possibility of only two consecutves after that, and having overall start and end
+        # https://stackoverflow.com/questions/51269456/pandas-delete-consecutive-duplicates-but-keep-the-first-and-last-value
+        keep_first_last=lambda s: s[~((s == s.shift(1)) & (s == s.shift(-1)))]
+
+        test=keep_first_last(textgridData.cmu_phones)
+
+        # keep firsts and lasts (thus only when there is two consecutive phonemes)
+        starts=test.loc[test.shift(-1) == test]
+        ends=test.loc[test.shift(+1) == test]
+
+        assert len(starts)==len(ends)
+
+        # drop duplicates keeping first
+        drop_duplicates=lambda a: a.loc[a.shift(+1) != a]
+        filtered_df=textgridData.loc[drop_duplicates(textgridData.cmu_phones).index]
+
+        # here in the filtered_df containg only the first occurence for equal consecutive examples, we replace the 'end' value with the ine in the "ends"
+        for rownum,(indx,val) in enumerate(starts.iteritems()): filtered_df.loc[indx,'end']=textgridData.loc[ends.index[rownum],'end']
+
+        # indxVowels=textgridData[textgridData.cmu_phones.isin(cmu_vowels)].index.tolist()
+        filtered_df=filtered_df[filtered_df.cmu_phones.isin(cmu_vowels)]#.index.tolist()
+
+        f0Samples=getIntonation(audio, self.sr)
+        intensity=getIntensity(audio, self.sr)
+
+        # extract features
+        # each word start and end position expressed in samples
+        startPositions_samples = (round(self.sr*filtered_df.iloc[:,0])+1).astype(int).tolist()
+        stopPositions_samples = round(self.sr*filtered_df.iloc[:,1]).astype(int).tolist()
+
+        # print('startPositions_samples',startPositions_samples)
+        # print('stopPositions_samples',stopPositions_samples)
+        
+        # to make sure we don t go beyond the end of the signal
+        assert stopPositions_samples[-1]<len(audio), "The end of the last phoneme should be inside the signal"
+
+        Imax,Imean,Fmax,Fmean,Dur=[],[],[],[],[]
+        # nVowels=len(indxVowels)
+        sylType=np.zeros(len(filtered_df))
+        for i in range(len(filtered_df)):
+            range_vowel=range(startPositions_samples[i], stopPositions_samples[i])
+            Ivowel=intensity[range_vowel]
+            Fvowel=f0Samples[range_vowel]
+            
+            Imax.append(max(Ivowel))
+            Imean.append(np.mean(Ivowel))
+            Fmax.append(max(Fvowel))
+            Fmean.append(np.mean(Fvowel))
+
+            Dur.append(filtered_df['end'].iloc[i]-filtered_df['start'].iloc[i])
+            
+            # phone_df=pd.DataFrame([r[2].split('_') for i,r in filtered_df.iterrows()])
+            # # here we use the prediction of HMM model as an indication, as it has to classify 0, 1 or 2
+            # syltype_phone=int(phone_df[2].iloc[i])
+            # if syltype_phone == 2:  # the sylType is 0 for unstressed, 0.5 for secondary stressed syllables and 1 for primary stressed syllables
+            #     sylType[i] = 0.5
+            # else:
+            #     sylType[i]=syltype_phone
+        
+        # normalization of features (projection to [0 1] range)
+        zImax = normalize(Imax)
+        zImean = normalize(Imean)
+        zFmax = normalize(Fmax)
+        zFmean = normalize(Fmean)
+        zDur = normalize(Dur)
+
+        # combine the features
+        # weighted_score = (zImax + 0.2*zImean + zFmax + 0.2*zFmean + 0.8*zDur + 0.4*sylType)/3.6  # needs fine-tuning once enough user data are available - in the long term train a classifier with annotated user data
+        weighted_score = (zImax + 0.2*zImean + zFmax + 0.2*zFmean + 0.8*zDur)/3.2  # needs fine-tuning once enough user data are available - in the long term train a classifier with annotated user data
+
+        return weighted_score
+
+
+
+
+
+
+if __name__=="__main__":
+    from utils.charsiu_utils import *
+    from transformers import Wav2Vec2Processor
+    from utils.libri_phonetization_data import libri_phonetics_data
+    import warnings
+    warnings.filterwarnings("ignore", category=UserWarning)
+
+    # from src.layer_extraction import get_last_hidden_state, get_logits, inference
+    # from scripts.wav2vec2_espeak import instances_per_phoneme, plot_reduction, phone_average_vectors
+    
+    # initialize model
+    charsiu = charsiu_phone_forced_aligner(aligner='charsiu/en_w2v2_fc_10ms', device='cpu')
+    df_t, df=libri_phonetics_data(data_set='dev-clean')
+    df_cmu_phones=df_t.apply(lambda r: pd.DataFrame.from_records(r.phone_df).cmu_phone.tolist(), axis=1)
+
+    # actor recordings
+    df=pd.read_csv('data/exercise_data_export.csv')
+    df['audio_file_url']='data/scaleway-audio-files/'+df['audio_file_url']
+    # those who don't have NaN in target
+    df_pContrast=df.loc[df.target_phoneme.dropna().index]
+    target_phones="IH0_D"
+    selection=df_pContrast[df_pContrast.target_phoneme==target_phones]
+    example=selection.iloc[10]
+
+    path=example.audio_file_url
+    s,fs=librosa.load(path, sr=16000)
+    phonetics=example.cmu_phonetics
+    split_phonetics=sum([p.replace('|','_').split('_') for p in phonetics.split(' ')],[])
+
+    df_segmented=charsiu.force_and_predict(s,split_phonetics)
+
+    charsiu.predict_phone(s, phonetics, 0, 1, 'D')
+
+    #  Inference with the example number N
+    N=0
+    example=df.iloc[N]
+    path=example.wav_path
+    print(example.phones)
+    example['cmu_phones']=df_cmu_phones.iloc[N]
+    # Loas an audio file
+    s,fs=librosa.load(path, sr=16000)
+
+    alignment = charsiu.align(audio=path,text=example.text)
+    print(alignment)
+    df_segmented=charsiu.force_and_predict(s,example.cmu_phones)
+    df_segmented=df_segmented[df_segmented.cmu_phones != '[SIL]']
+
+    # charsiu.compute_stress_score
+
+    df_segmented[df_segmented.cmu_phones!=df_segmented.pred_phones_audio]
+
+    ws=compute_stress_score(df_segmented, s, fs)
+
+    charsiu.compute_stress_score(s,example.cmu_phones)
+
+    path_to_json='/data/audio-with-analysis-ids/data.json'
+    d=pd.read_json(path_to_json)
+    
+
+
+
+    
+    # model=charsiu.__dict__["aligner"]
+    # processor = Wav2Vec2Processor.from_pretrained("facebook/wav2vec2-base")
+    # phone_outputs=phone_average_vectors(s, fs, df_segmented, processor, model, time_per_output=0.01, phone_type='cmu_phones', extractor_function=get_logits)
+    # phone_outputs=phone_average_vectors(s, fs, df_segmented, processor, model, time_per_output=0.01, phone_type='cmu_phones', extractor_function=inference)
+
+    # # define vectorized sigmoid
+    # sigmoid = np.vectorize(lambda x: 1 / (1 + math.exp(-x)))
+    # phone_outputs.apply(lambda r: sigmoid(r.average_vector), axis=1)
+
+    # # get_last_hidden_state(s, fs=16000, model=model)
+    # # get_hidden_states(s, fs=16000, model=model)
+    # # get_logits(s, fs=16000, model=model)
+
+    # df_all_instances=instances_per_phoneme(df_t, number_of_examples=len(df), time_per_output=0.01,  phone_type='cmu_phone', model=model)
+    # plot_reduction(df_all_instances, reduction_technique='umap', base_name='wav2vec2_frame_classification' )
+
+    # instances_per_phoneme(df_t, number_of_examples=10, time_per_output=0.01,  phone_type='cmu_phone', model=model)
+
+    # df_all_instances=instances_per_phoneme(df_t, processor, model, number_of_examples=10, time_per_output=0.02,  phone_type='cmu_phone', extractor_function=get_logits)
+
+
+    # -------------------------
+
+    # intialize model
+    # /!\  the get_hiddden_states and logits does not work with the following model. I don't know if it has something to do with attention mechanism
+    # charsiu = charsiu_attention_aligner('charsiu/en_w2v2_fs_10ms')
+    # alignment = charsiu.align(audio=audio_path,text=text)
+
+    # initialize model
+    charsiu_pred = charsiu_predictive_aligner(aligner='charsiu/en_w2v2_fc_10ms')
+    alignment = charsiu_pred.align(audio=path)
