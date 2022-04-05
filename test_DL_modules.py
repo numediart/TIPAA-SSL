b205c84018b50607cf60598dd2a442f443e4997d
noetits <noe.tits@gmail.com>
2022-04-05 19:38:06 +0300

    first kind of unit tests

---
+++ test_DL_modules.py
@@ -0,0 +1,7 @@
+from DL_accuracy_performance import *
+
+def test_pConstrast():
+    pContrast_for_actor_recordings(target_phones='AO1')
+
+def a_test_stress_detection():
+    stress_GE_performance_test(level='word')
