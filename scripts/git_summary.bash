4a53f59839351999af2202b2cbaf516131b392cf
noetits <noe.tits@gmail.com>
2022-04-06 15:42:59 +0300

    git summary from a specific commit

--- scripts/git_summary.bash
+++ scripts/git_summary.bash
@@ -1,2 +1,3 @@
 git fetch && git log --pretty=format:"%h %ad%x09%an%x09%s" --name-only --date=short HEAD..FETCH_HEAD>gitlog_from_main.txt
+git fetch && git log --pretty=format:"%h %ad%x09%an%x09%s" --name-only --date=short 227978e..HEAD>gitlog_from_commit.txt
 git log --pretty=format:"%h %ad%x09%an%x09%s" --name-only --date=short origin/main..HEAD>gitlog_local_commits.txt
