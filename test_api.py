be1c139e5b10d8e4dad6bf2e0d0033afc9664531
noetits <noe.tits@gmail.com>
2022-04-06 15:26:37 +0300

    content for annotation of user recordings

--- test_api.py
+++ test_api.py
@@ -6,13 +6,19 @@ import ast
 from tqdm import tqdm
 import shutil
 import numpy as np
+from collections import Counter
+import librosa
 import soundfile as sf
+
+
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning)
 
 def make_dir(path):
     if not os.path.exists(path): os.makedirs(path)
 
 def test_api(c=app.test_client()):
-    res=send_audio_base64(path='audio_recordings/ended.mp3', client=c)
+    res=send_audio_base64(path='data/audio_recordings/ended.mp3', client=c)
     assert res.status_code == 200
 
     res=ast.literal_eval(res.data.decode('utf-8'))
@@ -23,7 +29,7 @@ def test_api(c=app.test_client()):
 
     assert res.status_code == 200
 
-    res=send_audio_base64(path='audio_recordings/SS_1_i_would_love_to_go_to_ireland.wav', client=c)
+    res=send_audio_base64(path='data/audio_recordings/SS_1_i_would_love_to_go_to_ireland.wav', client=c)
 
     assert res.status_code == 200
     res=ast.literal_eval(res.data.decode('utf-8'))
@@ -59,6 +65,8 @@ def request_for_audio_file(r, base_url = 'http://localhost:8000', endpoint='/pho
     print(r['audio_file_url'])
 
     res=send_audio_base64(path=r['audio_file_url'], base_url = base_url, client=client)
+    print(res)
+    print(res.data)
     assert res.status_code == 200
     # This is for compatibility between requests module and flask's test_client
     if client==requests: res.data=res._content
@@ -67,7 +75,8 @@ def request_for_audio_file(r, base_url = 'http://localhost:8000', endpoint='/pho
     rID=res['rID']
     print(res)
 
-    if 'Stress' in endpoint:
+    # so that it works with Stress and stress
+    if 'tress' in endpoint:
         res = client.post(url, data={"phonetics":r['cmu_phonetics'],"text":r['text'], 'rID':rID, 
                                     'target':r['target_phoneme']})
     else:
@@ -82,6 +91,7 @@ def get_results(df_pContrast, base_url = 'http://localhost:8000', endpoint='/pho
     # client=app.test_client()
     # with app.test_client() as client:
     results=[]
+    print(len((df_pContrast))
     for i,r in tqdm(df_pContrast.iterrows()):
         res=request_for_audio_file(r, base_url = base_url, endpoint=endpoint, client=client)
         if '''"phonetic_detection":''' in res.data.decode('utf-8'):
@@ -206,10 +216,10 @@ def test_particular_cases(base_url = 'http://localhost:8000', endpoint='/phoneme
 
     return results
 
-def test_termination_contrast():
+def a_test_termination_contrast():
     test_GE_linguistic_data_content(endpoint='/terminationContrast')
 
-def test_user_recordings(base_url = 'http://localhost:8000', client=app.test_client(), n_audios=1, n_ex_by_ex_type=1, all_results_path='performance_results/user_recording_all_results.csv'):
+def a_test_user_recordings(base_url = 'http://localhost:8000', client=app.test_client(), n_audios=1, n_ex_by_ex_type=1, all_results_path='performance_results/user_recording_all_results.csv'):
     """[summary]
 
     Args:
@@ -221,11 +231,12 @@ def test_user_recordings(base_url = 'http://localhost:8000', client=app.test_cli
     Returns:
         [type]: [description]
     """
-    from user_audio_data import get_errors_examples, exercise_data
+    from utils.label_data_processing import get_errors_examples
+    exercise_data=pd.read_csv('data/flwc-recordings/QueryResultsForNoe-2021-12-23_120638.csv')
     # "cmu_phonetics", "target_word_indexes", 
     # "target_syllable_indexes", "alternative_phonemes", "target_phoneme"
     df_errors, exs_sort_by_n_errors=get_errors_examples()
-    
+
     endpoint_dict={
         'SENTENCE_STRESS':'/flowspeech/sentenceStress',
         'WORD_STRESS':'/flowspeech/wordStress',
@@ -290,9 +301,196 @@ def test_user_recordings(base_url = 'http://localhost:8000', client=app.test_cli
 
     return all_results_df
 
+def different_exercises():
+    # all_results=test_user_recordings(base_url = 'http://localhost:8000', client=requests, n_audios=1, n_ex_by_ex_type=5, all_results_path='performance_results/user_recording_all_results_local.csv')
+    test_GE_linguistic_data_content('http://localhost:8000', endpoint='/phonemeContrast', client=requests)
+    crash_test(base_url='http://localhost:8000')
+
+
+# from utils.label_data_processing import exercise_data, build_user_data_df
+from utils.label_data_processing import build_user_data_df
+exercise_data=pd.read_csv('data/flwc-recordings/QueryResultsForNoe-2021-12-23_120638.csv') 
+
+def pContrast_for_user_data( target_phones='AO1'):
+    user_data=build_user_data_df()
+    # user_data['module_type']=user_data.apply(lambda r: exercise_data[exercise_data.exercise_id==r.exercise_id].module_type.values[0], axis=1)
+    user_data['target_phoneme']=user_data.apply(lambda r: exercise_data[exercise_data.exercise_id==r.exercise_id].target_phoneme.values[0], axis=1)
+    # user_data['cmu_phonetics']=user_data.apply(lambda r: exercise_data[exercise_data.exercise_id==r.exercise_id].cmu_phonetics.values[0], axis=1)
+    # user_data['target_word_indexes']=user_data.apply(lambda r: exercise_data[exercise_data.exercise_id==r.exercise_id].target_word_indexes.values[0], axis=1)
+
+    selection=user_data[user_data.target_phoneme==target_phones]
+    selection['text']=selection.apply(lambda r: exercise_data[exercise_data.exercise_id==r.exercise_id].text.values[0], axis=1)
+    selection['cmu_phonetics']=selection.apply(lambda r: exercise_data[exercise_data.exercise_id==r.exercise_id].cmu_phonetics.values[0], axis=1)
+    selection['target_word_indexes']=selection.apply(lambda r: exercise_data[exercise_data.exercise_id==r.exercise_id].target_word_indexes.values[0], axis=1)
+    selection['target_syllable_indexes']=selection.apply(lambda r: exercise_data[exercise_data.exercise_id==r.exercise_id].target_syllable_indexes.values[0], axis=1)
+    selection['uid']=selection.apply(lambda r: r.exercise_id+r.audio_file_idx, axis=1)
+
+    selection['audio_file_url']=selection['fpath']
+    selections=[]
+    n_user=50
+    for u in selection.user_id.unique()[:n_user]:
+        for ex in selection.exercise_id.unique():
+            selections.append(selection[selection.user_id==u][selection.exercise_id==ex])
+    df=pd.concat(selections)
+
+    results=get_results(df)
+
+    df['phonetic_detection']=results['phonetic_detection']
+    df['status']=results['status']
+    df.to_csv('performance_results/vowel_contrast_user_data_'+target_phones+'.csv') 
+
+    d= results.phonetic_detection.value_counts()
+    d=d / d.sum()*100
+
+    return df
+
+
+def vowels_confusions_user_recordings():
+    vowels=['IY1','IH1','AO1','AA1','OW1']
+    predictions={}
+    for v in tqdm(vowels): 
+        # predictions[v]=pContrast_from_audiobook_data(data_set='dev-clean', target_phones=v, n=n)
+        predictions[v]=pContrast_for_user_data(target_phones=v)
+        # predictions[v].to_csv('performance_results/vowel_accuracies_'+v+'.csv')
+    
+    for v in tqdm(vowels): 
+        d=predictions[v].phonetic_detection.value_counts()
+        d=d/d.sum()*100
+        print(v+'\n', d)
+    
+    all_data=pd.concat(predictions)
+
+    cols=['exercise_id',
+       'processing_status', 'answer', 'user_id', 'audio_file_idx', 'fpath',
+       'fname', 'target_phoneme', 'text', 'cmu_phonetics', 'target_word_indexes',
+       'target_syllable_indexes', 'uid', 'audio_file_url',
+       'phonetic_detection', 'status']
+
+    all_data[cols].to_csv('performance_results/vowel_accuracies_all.csv', index=None)
+
+# ------------------------ This is for the annotation interface, This should be elsewhere --------------------
+cmudict_ref_words={
+    'AA' :'odd',
+    'AE' :'at',
+    'AH' :'hut',
+    'AO' :'ought',
+    'AW' :'cow',
+    'AY' :'hide',
+    'B' :'be',
+    'CH' :'cheese',
+    'D' :'dee',
+    'DH' :'thee',
+    'EH' :'Ed',
+    'ER' :'hurt',
+    'EY' :'ate',
+    'F' :'fee',
+    'G' :'green',
+    'HH' :'he',
+    'IH' :'it',
+    'IY' :'eat',
+    'JH' :'gee',
+    'K' :'key',
+    'L' :'lee',
+    'M' :'me',
+    'N' :'knee',
+    'NG' :'ping',
+    'OW' :'oat',
+    'OY' :'toy',
+    'P' :'pee',
+    'R' :'read',
+    'S' :'sea',
+    'SH' :'she',
+    'T' :'tea',
+    'TH' :'theta',
+    'UH' :'hood',
+    'UW' :'two',
+    'V' :'vee',
+    'W' :'we',
+    'Y' :'yield',
+    'Z' :'zee',
+    'ZH' :'seizure'
+}
+
+def user_data_to_turkle(base_static="../turkle/turkle/static/media/"):
+    all_data=pd.read_csv('performance_results/vowel_accuracies_all.csv')
+
+    # row=all_data[~all_data.status.str.contains('success')].iloc[0]
+    # selection=all_data[~all_data.status.str.contains('success')]
+    # results=[]
+    # for i,r in selection.iterrows():
+    #     res=request_for_audio_file(r, client=app.test_client())
+    #     results.append(res)
+    # row=all_data.iloc[0]
+    # res=request_for_audio_file(row, client=app.test_client())
+    # for i,r in all_data[all_data.audio_file_url.isin(rr.audio_file_url.tolist())]:
+    #     all_data.iloc[i].status=rr[rr.audio_file_url==all_data.iloc[i].audio_file_url].status.values[0]
+
+    all_data=all_data[~all_data.status.str.contains('voiced')][~all_data.status.str.contains('short')]
+
+    all_data.status.unique()
+
+    all_data['a1']=all_data['audio_file_url']
+
+    selection=all_data[['exercise_id', 'user_id', 'audio_file_idx', 'text','cmu_phonetics', 'target_phoneme','audio_file_url']]
+    selection.columns=['exercise_id', 'user_id', 'audio_file_idx', 'text','phonetics', 'target_phoneme','a1']
+
+    for i,r in tqdm(all_data.iterrows()):
+        add_path, fname=os.path.split('/'.join(r.a1.split('/')[1:]))
+        # full_path=os.path.join(base_static, add_path)selec
+        full_path=base_static
+        if not os.path.exists(full_path): os.makedirs(full_path)
+        # shutil.copy(r.a1, full_path)
+        s,fs=librosa.load(r.a1)
+        sf.write(full_path+'/'+fname.split('.')[0]+'.ogg', s, fs)
+    
+    selection.loc[:,'a1']=selection.apply(lambda r: 'media/'+ os.path.split(r.a1)[-1].split('.')[0]+'.ogg', axis=1)
+    selection.sample(frac=1, random_state=1234).to_csv('batch_all_selection.csv', index=None)
+
+    reference_path={}
+    reference_path['F_US']='scripts/synth_audio/cmu_words/standard/prosody/Joanna/'
+    reference_path['F_UK']='scripts/synth_audio/cmu_words/standard/prosody/Amy/'
+    for k in cmudict_ref_words: 
+        w=cmudict_ref_words[k]
+        p=reference_path['F_US']+'F_US'+'_'+w+'.mp3'
+        shutil.copy(p, full_path)
+        p=reference_path['F_UK']+'F_UK'+'_'+w+'.mp3'
+        shutil.copy(p, full_path)
+
+
+    
+    # selection=selection.iloc[:10]
+    selection=selection.sample(frac=0.01)
+    # selection.loc[:,'a1']=selection.apply(lambda r: '/media/'+ '/'.join(r.a1.split('/')[1:]), axis=1)
+    selection.loc[:,'a1']=selection.apply(lambda r: 'media/'+ os.path.split(r.a1)[-1].split('.')[0]+'.ogg', axis=1)
+    selection.to_csv('batch_test.csv', index=None)
+
+    return selection
+
+
+def html_balises_cmudict():
+    import cmudict
+    phone_df=pd.DataFrame(cmudict.phones())
+    vowels=phone_df[phone_df.apply(lambda r: r[1][0], axis=1)=='vowel'].iloc[:,0].tolist()
+
+    balises=[]
+    for v in vowels:
+        balise='''<input type="radio" name="vowel" id="'''+v+'''" value="'''+v+'''"><label for="'''+v+'''">'''+v+''' - '''+cmudict_ref_words[v]+'''</label>
+          <audio controls>
+            <source src="/static/media/F_US_'''+cmudict_ref_words[v]+'.mp3'+'''">
+            Your browser does not support HTML5 audio.
+          </audio>
+          <audio controls>
+            <source src="/static/media/F_UK_'''+cmudict_ref_words[v]+'.mp3'+'''">
+            Your browser does not support HTML5 audio.
+          </audio>
+          <br>'''
+        balises.append(balise)
+    print('\n'.join(balises))
+    return '\n'.join(balises)
+
 def detection_tests(all_results_path='performance_results/user_recording_all_results.csv'):
     all_results=pd.read_csv(all_results_path)
-    from utils.DL_speech_tech import run_VAD, load_audio_with_preprocessing, speech_enhancement
+    from utils.DL_audio_processing import run_VAD, load_audio_with_preprocessing, speech_enhancement
     probs=[]
     for i,r in tqdm(all_results.iterrows()):
         s,fs=load_audio_with_preprocessing(r[:-1].audio_file_url)
@@ -318,7 +516,7 @@ def detection_tests(all_results_path='performance_results/user_recording_all_res
 
     # all_results.to_csv(all_results_path)
 
-def user_recordings_results_analysis(all_results_path='performance_results/user_recording_all_results.csv'):
+def user_recordings_results_analysis(all_results_path='performance_results/user_recording_all_results_local.csv'):
 
     all_results=pd.read_csv(all_results_path)
 
@@ -395,20 +593,11 @@ def server_comparison_analysis():
 
     # len(rpC[~(r_ter==rpC).gibberish_detected])
 
-    # rpC[~(r_ter==rpC).gibberish_detected][rpC.target_phoneme=='D']
-    # r_ter[~(r_ter==rpC).gibberish_detected][r_ter.target_phoneme=='D']
-    # rpC[~(r_ter==rpC).gibberish_detected][rpC.target_phoneme=='T']
-    # r_ter[~(r_ter==rpC).gibberish_detected][r_ter.target_phoneme=='T']
-
+    res['rpC_local'][~(res['r_ter_local']==res['rpC_local']).gibberish_detected][res['rpC_local'].target_phoneme=='D']
+    res['r_ter_local'][~(res['r_ter_local']==res['rpC_local']).gibberish_detected][res['r_ter_local'].target_phoneme=='D']
     
-    # rpC[~(r_ter==rpC).gibberish_detected][rpC.target_phoneme.str.contains('AO')]
-    # r_ter[~(r_ter==rpC).gibberish_detected][r_ter.target_phoneme.str.contains('AO')]
-    # rpC[~(r_ter==rpC).gibberish_detected][rpC.target_phoneme.str.contains('OW')]
-    # r_ter[~(r_ter==rpC).gibberish_detected][r_ter.target_phoneme.str.contains('OW')]
-    # rpC[~(r_ter==rpC).gibberish_detected][rpC.target_phoneme.str.contains('IH')]
-    # r_ter[~(r_ter==rpC).gibberish_detected][r_ter.target_phoneme.str.contains('IH')]
-    # rpC[~(r_ter==rpC).gibberish_detected][rpC.target_phoneme.str.contains('IY')]
-    # r_ter[~(r_ter==rpC).gibberish_detected][r_ter.target_phoneme.str.contains('IY')]
+    res['rpC_local'][~(res['r_ter_local']==res['rpC_local']).gibberish_detected][res['rpC_local'].target_phoneme.str.contains('AO')]
+    res['r_ter_local'][~(res['r_ter_local']==res['rpC_local']).gibberish_detected][res['r_ter_local'].target_phoneme.str.contains('AO')]
 
 
 
@@ -416,13 +605,18 @@ if __name__ == '__main__':
     # test_api()
     # test_GE_linguistic_data_content()
     # rpC=test_GE_linguistic_data_content('http://localhost:8000/phonemeContrast')
+    
+
+    res=send_audio_base64(path='data/flwc-recordings/1AemA5FLTipViuoCLgorK/2021-12-15/yGWXA__P0__A1__1AemA5FLTipViuoCLgorK__1639600754876.m4a', base_url = 'https://dev-speech-processing.flowchase.app', client=requests)
 
     # rpC=test_GE_linguistic_data_content('https://dev-speech-processing.flowchase.app/phonemeContrast')
     # r_ter=test_GE_linguistic_data_content('http://localhost:8000/terminationContrast')
-    all_results=test_user_recordings(base_url = 'http://localhost:8000', client=app.test_client(), n_audios=20, n_ex_by_ex_type=5, all_results_path='performance_results/user_recording_all_results_local.csv')
-    # all_results=test_user_recordings(base_url = 'http://localhost:8000', client=app.test_client(), n_audios=20, n_ex_by_ex_type=5, all_results_path='performance_results/user_recording_all_results_SE.csv')
-    test_user_recordings()
-    all_results=test_user_recordings(base_url = 'https://dev-speech-processing.flowchase.app', client=requests, n_audios=20, n_ex_by_ex_type=5, all_results_path='performance_results/user_recording_all_results_dev.csv')
+    # all_results=test_user_recordings(base_url = 'http://localhost:8000', client=app.test_client(), n_audios=20, n_ex_by_ex_type=5, all_results_path='performance_results/user_recording_all_results_local.csv')
+    # # all_results=test_user_recordings(base_url = 'http://localhost:8000', client=app.test_client(), n_audios=20, n_ex_by_ex_type=5, all_results_path='performance_results/user_recording_all_results_SE.csv')
+    # test_user_recordings()
+    # all_results=test_user_recordings(base_url = 'https://dev-speech-processing.flowchase.app', client=requests, n_audios=20, n_ex_by_ex_type=5, all_results_path='performance_results/user_recording_all_results_dev.csv')
+    # all_results=test_user_recordings(base_url = 'https://dev-speech-processing.flowchase.app', client=requests, n_audios=1, n_ex_by_ex_type=5, all_results_path='performance_results/user_recording_all_results_dev.csv')
+    # all_results=test_user_recordings(base_url = 'http://localhost:8000', client=requests, n_audios=1, n_ex_by_ex_type=5, all_results_path='performance_results/user_recording_all_results_local.csv')
 
     rpC_dev=test_GE_linguistic_data_content('https://dev-speech-processing.flowchase.app', endpoint='/phonemeContrast', client=requests)
     r_ter_dev=test_GE_linguistic_data_content('https://dev-speech-processing.flowchase.app', endpoint='/terminationContrast', client=requests)
@@ -431,6 +625,19 @@ if __name__ == '__main__':
     r_ter_prod=test_GE_linguistic_data_content('https://speech-processing.flowchase.app', endpoint='/terminationContrast', client=requests)
 
     rpC_local=test_GE_linguistic_data_content('http://localhost:8000', endpoint='/phonemeContrast')
+    rpC_local['target_phoneme']=rpC_local.apply(lambda r: r.target_phoneme.replace('0','').replace('1','').replace('2',''), axis=1)
+    rpC_local['phonetic_detection']=rpC_local.apply(lambda r: r.phonetic_detection.replace('0','').replace('1','').replace('2',''), axis=1)
+    targets=rpC_local.target_phoneme.unique()
+    for t in targets:
+        d=Counter(rpC_local[rpC_local.target_phoneme==t].phonetic_detection.tolist())
+        df = pd.DataFrame.from_dict(d, orient='index')
+        df.columns=[t]
+        df=df.sort_values(by=t, ascending=False)
+        df=df / df.sum()*100
+        print(df)
+        
+
+
     r_ter_local=test_GE_linguistic_data_content('http://localhost:8000', endpoint='/terminationContrast')
     
     test_GE_linguistic_data_content()
