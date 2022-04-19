99e33bb3305b9c4ec5fa90deb66ccc70bfd3ef96
noetits <noe.tits@gmail.com>
2022-04-19 16:13:07 +0300

    unit tests for new endpoints

--- test_DL_api.py
+++ test_DL_api.py
@@ -10,6 +10,8 @@ warnings.filterwarnings("ignore", category=UserWarning)
 
 from test_api import get_results, request_for_audio_file
 from utils.label_data_processing import actor_recordings
+from DL_accuracy_performance import count_values, plot_confusion_results
+
 
 
 def get_results(df_pContrast, base_url = 'http://localhost:8000', endpoint='/w2v/contrast/vowel', client=app.test_client()):
@@ -33,7 +35,7 @@ def get_results(df_pContrast, base_url = 'http://localhost:8000', endpoint='/w2v
     return results_df, failures
 
 
-def test_GE_linguistic_data_content(base_url = 'http://localhost:8000', client=app.test_client(), n_ex_by_module=10):
+def test_actor_recordings(base_url = 'http://localhost:8000', client=app.test_client(), n_ex_by_module=10):
     df=actor_recordings()
 
     df_pContrast=df.loc[df.target_phoneme.dropna().index]
@@ -48,20 +50,31 @@ def test_GE_linguistic_data_content(base_url = 'http://localhost:8000', client=a
 
     route='/w2v/contrast/'
     results_v, failures_v=get_results(df_v.iloc[:n_ex_by_module,:], base_url = base_url, endpoint=route+'vowel', client=client)
-    results_ed, failures_ed=get_results(df_ed.iloc[:n_ex_by_module,:], base_url = base_url, endpoint=route+'syllable', client=client)
+    results_ed_s, failures_ed_s=get_results(df_ed.iloc[:n_ex_by_module,:], base_url = base_url, endpoint=route+'syllable', client=client)
+    results_ed, failures_ed=get_results(df_ed.iloc[:n_ex_by_module,:], base_url = base_url, endpoint=route+'termination', client=client)
 
-    results_v[results_v.gibberish_truth!=results_v.gibberish_detected]
-    results_ed[results_ed.gibberish_truth!=results_ed.gibberish_detected]
+    results_v[results_v.phonetic_detection!=results_v.target_phoneme]
     
+    results_ed[results_ed.phonetic_detection!=results_ed.target_phoneme]
+
+    success_rate = lambda df : len(df[df.phonetic_detection==df.target_phoneme])/len(df) if len(df)>0 else float('nan')
+    success_rate(results_ed)
+    success_rate(results_v)
+
     for v in vowels:
         res=results_v[results_v.target_phoneme.str.contains(v)]
-        errors=results_v[results_v.gibberish_truth!=results_v.gibberish_detected][results_v.target_phoneme.str.contains(v)&~results_v.target_phoneme.str.contains('_')]
-        if len(res)>0:
-            print('Target is', v, 'error rate:', len(errors)/len(res), 'n:', len(res))
-            confusion_ps=errors.phonetic_detection.unique()
-            for p in confusion_ps:
-                error_part=res[res.phonetic_detection==p]
-                print(p, len(error_part)/len(res))
+        print(count_values(res[res.phonetic_detection!=res.target_phoneme].phonetic_detection))
+        print('success rate for '+v, success_rate(res))
+    for t in eds:
+        res=results_ed[results_ed.target_phoneme.str.contains(t)]
+        print(count_values(res[res.phonetic_detection!=res.target_phoneme].phonetic_detection))
+        print('success rate for '+t, success_rate(res))
+    for t in eds:
+        res=results_ed_s[results_ed_s.target_phoneme.str.contains(t)]
+        print(count_values(res[res.gibberish_detected!=res.gibberish_truth].gibberish_detected))
+        success_rate = lambda df : len(df[df.gibberish_truth==df.gibberish_detected])/len(df) if len(df)>0 else float('nan')
+        print('success rate for '+t, success_rate(res))
+
 
 
 def test_stress_detection(base_url = 'http://localhost:8000', client=app.test_client(), n_ex_by_module=10):
@@ -71,17 +84,16 @@ def test_stress_detection(base_url = 'http://localhost:8000', client=app.test_cl
     df_sentence_stress=df.loc[df.stress_category.dropna().index]
     df_word_stress=df.drop(df_pContrast.index).drop(df_sentence_stress.index)
 
-    
     results_ss, failures_ss=get_results(df_sentence_stress.iloc[:n_ex_by_module,:], base_url = base_url, endpoint='/w2v/stress/sentence', client=client)
     results_ws, failures_ws=get_results(df_word_stress.iloc[:n_ex_by_module,:], base_url = base_url, endpoint='/w2v/stress/word', client=client)
-
+    
 
 
 # from utils.label_data_processing import exercise_data, build_user_data_df
 from utils.label_data_processing import build_user_data_df
-exercise_data=pd.read_csv('data/flwc-recordings/QueryResultsForNoe-2021-12-23_120638.csv') 
+exercise_data=pd.read_csv('data/flwc-recordings/QueryResultsForNoe-2021-12-23_120638.csv')
 
-def pContrast_for_user_data( target_phones='AO1', n_user=50):
+def pContrast_for_user_data( target_phones='AO1', n_user=10, n_ex_by_ex_type=10):
     user_data=build_user_data_df()
     # user_data['module_type']=user_data.apply(lambda r: exercise_data[exercise_data.exercise_id==r.exercise_id].module_type.values[0], axis=1)
     user_data['target_phoneme']=user_data.apply(lambda r: exercise_data[exercise_data.exercise_id==r.exercise_id].target_phoneme.values[0], axis=1)
@@ -100,23 +112,43 @@ def pContrast_for_user_data( target_phones='AO1', n_user=50):
     
     for u in selection.user_id.unique()[:n_user]:
         for ex in selection.exercise_id.unique():
-            selections.append(selection[selection.user_id==u][selection.exercise_id==ex])
+            selections.append(selection[selection.user_id==u][selection.exercise_id==ex][:n_ex_by_ex_type])
     df=pd.concat(selections)
 
-    results=get_results(df)
+    results_df, failures=get_results(df)
 
-    df['phonetic_detection']=results['phonetic_detection']
-    df['status']=results['status']
-    df.to_csv('performance_results/vowel_contrast_user_data_'+target_phones+'.csv') 
-
-    d= results.phonetic_detection.value_counts()
-    d=d / d.sum()*100
+    df['phonetic_detection']=results_df['phonetic_detection']
+    df['status']=results_df['status']
+    df.to_csv('performance_results/vowel_contrast_user_data_'+target_phones+'_w2v.csv') 
+    
+    d=count_values(results_df.phonetic_detection.tolist())
+    d.columns=[target_phones]
 
     return df, d
 
 
+def vowels_confusions_user_recordings(n_user=10, n_ex_by_ex_type=10):
+    vowels=['IY1','IH1','AO1','AA1','OW1']
+    results={}
+    for v in vowels: 
+        print("vowel:",v)
+        _, rates=pContrast_for_user_data(target_phones=v, n_user=n_user, n_ex_by_ex_type=n_ex_by_ex_type)
+        # pickle.dump(predictions[v], open('vowel_accuracies'+v+'.p','wb'))
+        results[v]=rates
+    return results
 
 if __name__ == '__main__':
+    test_actor_recordings(base_url = 'http://localhost:8000', client=requests, n_ex_by_module=10)
+
+    from test_DL_api import *;print(pContrast_for_user_data())
+
+    import time
+    start=time.time()
+    results=vowels_confusions_user_recordings()
+    plot_confusion_results(results, name='vowel_contrast_user_data_w2v_w_speech_corr')
+    duration=time.time()-start
+    print('duration:', duration) # 1800s = 30m
+
     # test_api()
     r=test_GE_linguistic_data_content()
     
@@ -128,6 +160,8 @@ if __name__ == '__main__':
     # try with local docker container
     test_stress_detection(client=requests)
     test_GE_linguistic_data_content(client=requests)
+
+    test_GE_linguistic_data_content(base_url = 'http://146.59.241.79/', client=requests)
 
     df, d=pContrast_for_user_data()
     print(d)
