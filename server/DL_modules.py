056677d0a5bcff75d426752b61824de3951fc239
noetits <noe.tits@gmail.com>
2022-04-19 16:12:27 +0300

    -added new endpoints for consonant contrast and termination contrast -refactored request_phoneme_contrast, the common core request for contrasts

--- server/DL_modules.py
+++ server/DL_modules.py
@@ -4,9 +4,9 @@ from marshmallow import Schema, fields
 from flask_apispec import marshal_with, doc, use_kwargs
 from flask import Response
 
-from DL_speech_tech import phonemeContrast_from_formatted_phonetics_audio, stress_from_formatted_phonetics, syllable_contrast_from_formatted_phonetics_audio
+from DL_speech_tech import phonemeContrast_from_formatted_phonetics_audio, stress_from_formatted_phonetics, termination_contrast_from_formatted_phonetics_audio, syllable_contrast_from_formatted_phonetics_audio
 import json
-from utils.text_processing import check_phonemes
+from utils.text_processing import check_phonemes, cmu_vowels, cmu_consonants
 
 from app_definition import app
 from server.utils import access_property_error, properties_to_args
@@ -38,8 +38,8 @@ properties=["phonetics","rID","text"]
 @app.route('/w2v/stress/<module>', methods=['POST'])
 def dl_module_api(module):
     d = request.form
-    print(d)
     properties=["phonetics","rID","text"]
+
     err=check_request(d, properties)
     if err is not None: return Response(err,status=400,mimetype="application/json")
 
@@ -58,6 +58,27 @@ def dl_module_api(module):
         return Response(response,status=200,mimetype="application/json")
 
 
+def request_phoneme_contrast(d, properties, target_occurence_idx=0, tech_function=phonemeContrast_from_formatted_phonetics_audio, alternatives=cmu_vowels):
+    err=check_request(d, properties)
+    if err is not None: return Response(err,status=400,mimetype="application/json")
+    word_idx=int(d['word_idx'])
+    syl_idx=int(d['syl_idx'])
+    if word_idx>=len(d['phonetics'].split(' ')):
+        return Response("error: word_idx >= number of words",status=400,mimetype="application/json")
+    # extract the syllables which contain the target
+    syls_with_target=[syl for syl in d['phonetics'].split(' ')[word_idx].split('|') if d['target'] in syl]
+    idx_syls_with_target=[i for i,syl in enumerate(d['phonetics'].split(' ')[word_idx].split('|')) if d['target'] in syl]
+
+    if syl_idx not in idx_syls_with_target:
+        return Response("error: the syllable corresponding to syl_idx does not contain the target.",status=400,mimetype="application/json")
+    
+    res=tech_function(d['rID'], d['phonetics'], target_word_idx=word_idx, target_syllable_idx=syl_idx, target_occurence_idx=target_occurence_idx, target_phones=d['target'], alternatives=alternatives)
+    response=json.dumps(res)
+
+    if res['status'].split(':')[0]=='error':
+        return Response(response,status=500,mimetype="application/json")
+    else:
+        return Response(response,status=200,mimetype="application/json")
 
 responseSchema=Schema.from_dict(
     {"status": fields.Str(), 
@@ -71,32 +92,47 @@ properties=["phonetics","rID","word_idx","target","syl_idx"]
 @use_kwargs(properties_to_args(properties), location=('form'))
 @marshal_with(responseSchema, code=200)  # marshalling
 @app.route('/w2v/contrast/vowel', methods=['POST'])
-def dl_phoneme_contrast_api():
+def dl_vowel_contrast_api():
     d = request.form
     properties=["phonetics","rID","word_idx","target","syl_idx"]
-    err=check_request(d, properties)
-    if err is not None: return Response(err,status=400,mimetype="application/json")
+    return request_phoneme_contrast(d, properties)
 
-    word_idx=int(d['word_idx'])
-    syl_idx=int(d['syl_idx'])
 
-    if word_idx>=len(d['phonetics'].split(' ')):
-        return Response("error: word_idx >= number of words",status=400,mimetype="application/json")
-    
-    # extract the syllables which contain the target
-    syls_with_target=[syl for syl in d['phonetics'].split(' ')[word_idx].split('|') if d['target'] in syl]
-    idx_syls_with_target=[i for i,syl in enumerate(d['phonetics'].split(' ')[word_idx].split('|')) if d['target'] in syl]
+responseSchema=Schema.from_dict(
+    {"status": fields.Str(), 
+    "phonetic_detection": fields.Str(),
+    "gibberish_truth": fields.Str(),
+    "gibberish_detected":fields.Str()},
+    name="consonant_contrast_response"
+)
+properties=["phonetics","rID","word_idx","target","syl_idx", "target_occurence_idx"]
+@doc(description='Consonant contrast', tags=['consonant_contrast'])
+@use_kwargs(properties_to_args(properties), location=('form'))
+@marshal_with(responseSchema, code=200)  # marshalling
+@app.route('/w2v/contrast/consonant', methods=['POST'])
+def dl_consonant_contrast_api():
+    d = request.form
+    properties=["phonetics","rID","word_idx","target","syl_idx", "target_occurence_idx"]
+    target_occurence_idx=int(d['target_occurence_idx'])
+    return request_phoneme_contrast(d, properties, target_occurence_idx, alternatives=cmu_consonants)
 
-    if syl_idx not in idx_syls_with_target:
-        return Response("error: the syllable corresponding to syl_idx does not contain the target.",status=400,mimetype="application/json")
-    
-    res=phonemeContrast_from_formatted_phonetics_audio(d['rID'], d['phonetics'], target_word_idx=word_idx, target_syllable_idx=syl_idx, target_phones=d['target'])
-    response=json.dumps(res)
 
-    if res['status'].split(':')[0]=='error':
-        return Response(response,status=500,mimetype="application/json")
-    else:
-        return Response(response,status=200,mimetype="application/json")
+responseSchema=Schema.from_dict(
+    {"status": fields.Str(), 
+    "phonetic_detection": fields.Str(),
+    "gibberish_truth": fields.Str(),
+    "gibberish_detected":fields.Str()},
+    name="termination_contrast_response"
+)
+properties=["phonetics","rID","word_idx","target","syl_idx", "target_occurence_idx"]
+@doc(description='Termination contrast', tags=['termination_contrast'])
+@use_kwargs(properties_to_args(properties), location=('form'))
+@marshal_with(responseSchema, code=200)  # marshalling
+@app.route('/w2v/contrast/termination', methods=['POST'])
+def dl_termination_contrast_api():
+    d = request.form
+    properties=["phonetics","rID","word_idx","target"]
+    return request_phoneme_contrast(d, properties, tech_function=termination_contrast_from_formatted_phonetics_audio)
 
 
 
@@ -112,6 +148,7 @@ properties=["phonetics","rID","word_idx","syl_idx"]
 @marshal_with(responseSchema, code=200)  # marshalling
 @app.route('/w2v/contrast/syllable', methods=['POST'])
 def dl_syllable_contrast_api():
+    # TODO: refactor with "request_phoneme_contrast" by parametrizing the function call
     d = request.form
     properties=["phonetics","rID","word_idx","syl_idx"]
     err=check_request(d, properties)
