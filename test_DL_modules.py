a1bf69a3d84e75b24529ad19e17c9a029853526d
noetits <noe.tits@gmail.com>
2022-04-19 16:09:11 +0300

    unit tests for termination contrast

--- test_DL_modules.py
+++ test_DL_modules.py
@@ -2,6 +2,9 @@ from DL_accuracy_performance import *
 
 def test_pConstrast():
     pContrast_for_actor_recordings(target_phones='AO1')
+def test_termination_contrast():
+    termination_contrast_from_audiobook_data(data_set='test-other', target_phones='D', n=50)
+    termination_contrast_for_actor_recordings()
 
 def a_test_stress_detection():
     stress_GE_performance_test(level='word')
