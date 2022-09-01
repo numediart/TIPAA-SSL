dbbd0f85ff90a6097c1ce964623c9bf9af0fe345
noetits <noe.tits@gmail.com>
2022-09-01 15:39:31 +0300

    use gunicorn, now that we have traceback directly in python and can put it in json

--- run_server.sh
+++ run_server.sh
@@ -6,9 +6,8 @@
 # gunicorn flask_server:app 
 # uwsgi --http 127.0.0.1:5000 --module flask_server:app
 
-export FLOWSPEECH_KEY="ThisIsTheFlowchaseSP-APIKey:MeaningOfLife=42"
 cron
-# gunicorn -b 0.0.0.0:8000 flask_server:app
 mfa model download g2p french_mfa && mfa model download g2p spanish_spain_mfa && mfa model download g2p spanish_latin_america_mfa && mfa model download g2p english_uk_mfa && mfa model download g2p english_us_mfa  
-NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program python flask_server.py
+gunicorn -b 0.0.0.0:8000 flask_server:app --timeout 90 
+# NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program python flask_server.py
 # NEW_RELIC_CONFIG_FILE=newrelic.ini newrelic-admin run-program gunicorn -b 0.0.0.0:8000 flask_server:app
