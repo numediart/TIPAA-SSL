b965dce3dfa42b7e541191cb1e5e7dc121842734
Noé Tits <noe.tits@gmail.com>
2022-11-30 04:39:19 +0100

    comment downloads that are not for production but only to try locally

--- scripts/download_models.py
+++ scripts/download_models.py
@@ -15,7 +15,8 @@ hf_rep='charsiu'
 download_hf_model(hf_path_base=hf_path_base, hf_rep=hf_rep, hf_model='en_w2v2_fc_10ms')
 download_hf_model(hf_path_base=hf_path_base, hf_rep=hf_rep, hf_model='tokenizer_en_cmu')
 download_hf_model(hf_path_base=hf_path_base, hf_rep=hf_rep, hf_model='tokenizer_zh_pinyin')
-download_hf_model(hf_path_base=hf_path_base, hf_rep="speechbrain", hf_model="metricgan-plus-voicebank")
+# download_hf_model(hf_path_base=hf_path_base, hf_rep="speechbrain", hf_model="metricgan-plus-voicebank")
 
 download_hf_model(hf_path_base=hf_path_base, hf_rep="facebook", hf_model="wav2vec2-xlsr-53-espeak-cv-ft")
-download_hf_model(hf_path_base=hf_path_base, hf_rep="facebook", hf_model="wav2vec2-xlsr-53-espeak-cv-ft")
+# download_hf_model(hf_path_base=hf_path_base, hf_rep="facebook", hf_model="wav2vec2-large-xlsr-53")
+# download_hf_model(hf_path_base=hf_path_base, hf_rep="facebook", hf_model="wav2vec2-base-960h")
