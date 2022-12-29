ef9888ce42c079616865b048f40f4be396d363b7
Noé Tits <noe.tits@gmail.com>
2022-12-29 11:47:18 +0100

    utils for content tools

---
+++ content_tools/utils.py
@@ -0,0 +1,64 @@
+# Copyright 2018-2022 Streamlit Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import streamlit as st
+import inspect
+import textwrap
+
+from transformers import Wav2Vec2ForCTC
+
+@st.experimental_singleton
+def get_model(model_type):
+    # Create a model of the specified type
+    return Wav2Vec2ForCTC.from_pretrained(model_type)
+
+
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password"] == st.secrets["password"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # don't store password
+        else:
+            st.session_state["password_correct"] = False
+
+    if "password_correct" not in st.session_state:
+        # First run, show input for password.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        return False
+    elif not st.session_state["password_correct"]:
+        # Password not correct, show input + error.
+        st.text_input(
+            "Password", type="password", on_change=password_entered, key="password"
+        )
+        st.error("😕 Password incorrect")
+        return False
+    else:
+        # Password correct.
+        return True
+
+
+def show_code(demo):
+    """Showing the code of the demo."""
+    show_code = st.sidebar.checkbox("Show code", True)
+    if show_code:
+        # Showing the code of the demo.
+        st.markdown("## Code")
+        sourcelines, _ = inspect.getsourcelines(demo)
+        st.code(textwrap.dedent("".join(sourcelines[1:])))
