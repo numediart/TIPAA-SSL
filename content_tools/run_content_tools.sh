d6c22361eecd2a3dd59a61cf3a5b39b62a711cc8
Noé Tits <noe.tits@gmail.com>
2023-01-12 17:00:54 +0100

    remove comment line

--- content_tools/run_content_tools.sh
+++ content_tools/run_content_tools.sh
@@ -1,3 +1,2 @@
 cron
 streamlit run content_tools/Hello.py --server.port 8501 --server.enableCORS=false --server.enableXsrfProtection=false
-# streamlit run content_tools/Hello.py --server.enableXsrfProtection=false --server.enableWebsocketCompression=false --server.enableCORS=false
