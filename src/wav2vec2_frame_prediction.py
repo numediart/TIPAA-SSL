30dce2ba8d93aab5d864462c1ed03905618ecd49
Noé Tits <noe.tits@gmail.com>
2023-02-08 09:34:14 +0100

    allow using either torch or onnx model from a path to output last hidden layer

--- src/wav2vec2_frame_prediction.py
+++ src/wav2vec2_frame_prediction.py
@@ -10,24 +10,14 @@ import ast
 
 
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.neural_network import MLPClassifier
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.gaussian_process import GaussianProcessClassifier
-from sklearn.gaussian_process.kernels import RBF
-from sklearn.naive_bayes import GaussianNB
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-from sklearn.linear_model import LogisticRegression
 
-from sklearn.preprocessing import LabelEncoder
+
+from sklearn.neighbors import KNeighborsClassifier
+
 from src.audio_processing import getIntonation, getIntensity, normalize
-from src.text_processing import unstress, prefill_for_sentence, remove_stress_annots
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split
+from src.text_processing import unstress, remove_stress_annots
 from sklearn.decomposition import PCA
-from umap.umap_ import UMAP
+# from umap.umap_ import UMAP
 
 
 from time import time
@@ -60,7 +50,10 @@ def extract_word(df_segmented, phonetics, target_word_idx):
 class Wav2Vec2ForFramePrediction:
 
     # phone_type = 'cmu' or 'ipa'
-    def __init__(self, phone_type, w2v2_model_path="hf_models/facebook/wav2vec2-xlsr-53-espeak-cv-ft",  reducer=PCA(n_components=0.95, random_state=42), frame_classifier=KNeighborsClassifier(10)):#, phoneme_classifier=None):
+    def __init__(self, phone_type, w2v2_model_path="hf_models/facebook/wav2vec2-xlsr-53-espeak-cv-ft", w2v2_model_format="torch", reducer=PCA(n_components=0.95, random_state=42), frame_classifier=KNeighborsClassifier(10)):#, phoneme_classifier=None):
+        """
+        w2v_format: 'torch' or 'onnx'
+        """
         self.status = 'success'
         self.pred_phones_audio = []
         self.fs = 16000
@@ -85,7 +78,17 @@ class Wav2Vec2ForFramePrediction:
 
 
         # import Wav2Vec2 feature extractor
-        self.model = Wav2Vec2Model.from_pretrained(w2v2_model_path, output_hidden_states=True) 
+        self.w2v2_model_format=w2v2_model_format
+        if w2v2_model_format=="torch":
+            self.model = Wav2Vec2Model.from_pretrained(w2v2_model_path, output_hidden_states=True) 
+        else:
+            import onnxruntime as rt
+            quantized_model_name = "last_hidden_state.quant.onnx"
+            quantized_model_path='/'.join([w2v2_model_path,quantized_model_name])
+            sess_options = rt.SessionOptions()
+            sess_options.graph_optimization_level = rt.GraphOptimizationLevel.ORT_ENABLE_ALL
+            self.session = rt.InferenceSession(quantized_model_path, sess_options)
+
         self.processor = Wav2Vec2Processor.from_pretrained(w2v2_model_path)    
 
     def save(self, out_path='models', name='model_mailabs_pca_0.95_knn_10_w'):
@@ -103,8 +106,13 @@ class Wav2Vec2ForFramePrediction:
     # get output from an audio sample in w2v2 feature extractor
     def get_last_hidden_state(self, s, fs):
         input_values = self.processor(torch.tensor(s), sampling_rate=fs, return_tensors="pt").input_values.to('cpu')
-        with torch.no_grad(): 
-            return self.model(input_values).hidden_states[-1]
+
+        if self.w2v2_model_format=="torch":
+            with torch.no_grad(): 
+                return self.model(input_values).hidden_states[-1]
+        else:
+            onnx_outputs = self.session.run(None, {self.session.get_inputs()[0].name: input_values.numpy()})[0]
+            return onnx_outputs
 
     # reduce dimension from w2v2 output
     def reduce_lhs_dimension(self, lhs):
@@ -163,11 +171,11 @@ class Wav2Vec2ForFramePrediction:
 
         df_segmented=self.forced_aligner.probas_to_df_segmented(phone_prob_matrix, target_phonemes, fs=self.fs)
 
-        avg_vectors=[]
-        for i,r in df_segmented.iterrows():
-            avg_vector=self.lhs.numpy()[0][r.start_idx:r.end_idx,:].mean(axis=0)
-            avg_vectors.append(avg_vector)
-        df_segmented['average_vectors']=avg_vectors
+        # avg_vectors=[]
+        # for i,r in df_segmented.iterrows():
+        #     avg_vector=self.lhs.numpy()[0][r.start_idx:r.end_idx,:].mean(axis=0)
+        #     avg_vectors.append(avg_vector)
+        # df_segmented['average_vectors']=avg_vectors
 
         self.pred_phones_audio = list(df_segmented.pred_phones_audio.values)
 
@@ -342,6 +350,9 @@ class Wav2Vec2ForFramePrediction:
 
 
 def train_Wav2Vec2ForFramePrediction_model():
+    
+
+    from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis, LinearDiscriminantAnalysis
     # Basis model Wav2Vec2ForFramePrediction, a 10-NN classifier weighted with distances
     df_all_frames=pd.read_pickle('df_all_frames_MAILABS_train.pkl')
     X, y = df_all_frames_to_X_y(df_all_frames)
@@ -364,6 +375,38 @@ def train_Wav2Vec2ForFramePrediction_model():
     model = Wav2Vec2ForFramePrediction('cmu', reducer=PCA(n_components=0.99, random_state=42), frame_classifier=KNeighborsClassifier(5, weights='distance', metric='cosine'))
     model.fit(X, y)
     model.save(name='model_mailabs_pca_99_knn_5_cos_w')
+
+    
+    # Basis model Wav2Vec2ForFramePrediction, COMMON VOICE DATA
+    df_all_frames=pd.read_pickle('df_all_frames_commonvoice_en_dev.pkl')
+    X, y = df_all_frames_to_X_y(df_all_frames)
+    model = Wav2Vec2ForFramePrediction('cmu', reducer=PCA(n_components=0.99, random_state=42), frame_classifier=LinearDiscriminantAnalysis())
+    model.fit(X, y)
+    model.save(name='model_commonvoice_pca_99_lda')
+
+    
+    # Basis model Wav2Vec2ForFramePrediction, COMMON VOICE DATA
+    df_all_frames=pd.read_pickle('df_all_frames_commonvoice_en_dev.pkl')
+    X, y = df_all_frames_to_X_y(df_all_frames)
+    model = Wav2Vec2ForFramePrediction('cmu', reducer=PCA(n_components=0.99, random_state=42), frame_classifier=KNeighborsClassifier(10, weights='distance', metric='cosine'))
+    model.fit(X, y)
+    model.save(name='model_commonvoice_pca_99_knn')
+
+    
+    # Basis model Wav2Vec2ForFramePrediction, COMMON VOICE DATA
+    df_all_frames=pd.read_pickle('df_all_frames_commonvoice_en_dev_uk_us_ca_n_1000.pkl')
+    X, y = df_all_frames_to_X_y(df_all_frames)
+    model = Wav2Vec2ForFramePrediction('cmu', reducer=PCA(n_components=0.99, random_state=42), frame_classifier=KNeighborsClassifier(10, weights='distance', metric='cosine'))
+    model.fit(X, y)
+    model.save(name='model_commonvoice_pca_99_knn_uk_us_ca_n_1000')
+
+    
+    # Basis model Wav2Vec2ForFramePrediction, COMMON VOICE DATA
+    df_all_frames=pd.read_pickle('df_all_frames_commonvoice_en_dev_uk_us_ca_n_1000.pkl')
+    X, y = df_all_frames_to_X_y(df_all_frames)
+    model = Wav2Vec2ForFramePrediction('cmu', reducer=PCA(n_components=0.99, random_state=42), frame_classifier=LinearDiscriminantAnalysis())
+    model.fit(X, y)
+    model.save(name='model_commonvoice_pca_99_lda_uk_us_ca_n_1000')
 
     
     # model Wav2Vec2ForFramePrediction, but a 10-NN classifier weighted with distances, UK US FR ES, IPA
@@ -396,9 +439,46 @@ def train_Wav2Vec2ForFramePrediction_model():
     # eclf = VotingClassifier(estimators=estimators, voting='soft', weights=[1,5])
     # reducer=PCA(n_components=0.99, random_state=42)
 
+def inference_demo():
+    
+    # --------------- Inference demo --------------------
+    
+    # phoneme predictions on a train dataset with forced alignment
+    # comment for cmu or ipa
+    df_t_train, df_t_test = load_libri_dataset()
+    data = load_cmu_test_dataset(df_t_test)
+    # data = load_test_dataset(df_t_test)
 
+    # # phoneme predictions on a single audio sample with forced alignment
+    # pred = model.predict_with_timings(data.s.iloc[0], data.cmu_phones.iloc[0])
+    # prob_matrix = model.predict_phone_prob_matrix(data.s.iloc[0], 16000)
+
+    
+    from src.wav2vec2_frame_prediction import Wav2Vec2ForFramePrediction
+    # default_model_cmu = Wav2Vec2ForFramePrediction('cmu')
+    default_model_cmu = Wav2Vec2ForFramePrediction('cmu',w2v2_model_format="onnx")
+    default_model_cmu.load(name='model_mailabs_pca_0.95_knn_10_w')
+
+    # phoneme predictions on a single audio sample with forced alignment
+    pred = default_model_cmu.predict_with_timings(data.s.iloc[0], data.cmu_phones.iloc[0])
+    prob_matrix = default_model_cmu.predict_phone_prob_matrix(data.s.iloc[0], 16000)
+    return pred, prob_matrix
 
 if __name__ == '__main__':
+
+    
+    # from sklearn.model_selection import train_test_split
+    # from sklearn.preprocessing import StandardScaler
+    # from sklearn.neural_network import MLPClassifier
+    # from sklearn.svm import SVC
+    # from sklearn.gaussian_process import GaussianProcessClassifier
+    # from sklearn.gaussian_process.kernels import RBF
+    # from sklearn.naive_bayes import GaussianNB
+
+    from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
+
+    
+    from sklearn.linear_model import LogisticRegression
 
     # backup different possible reducers
     # if reducer == "umap":
@@ -411,8 +491,25 @@ if __name__ == '__main__':
     #     self.reducer = PCA(n_components=target_dim, random_state=42)
 
     from src.load_data import *
-    # from src.wav2vec2_frame_prediction import *
-    from src.wav2vec2_frame_prediction import Wav2Vec2ForFramePrediction
+    from src.wav2vec2_frame_prediction import *
+    # from src.wav2vec2_frame_prediction import Wav2Vec2ForFramePrediction
+
+    df_t=load_dataset_commonvoice(lang_codes=['en'], path='./data/cv-corpus-10.0-delta-2022-07-04', split="dev", phone_set='CMU')
+    df_t.accents.unique()
+    accents=['United States English']
+    # accents=['England English']
+    df_t=df_t[df_t.accents.isin(accents)]
+    df_all_frames = build_df_all_frames(df_t.sample(frac=1, random_state=0), 'phone')
+    df_all_frames.to_pickle('df_all_frames_commonvoice_en_dev_us.pkl')
+    
+    # Basis model Wav2Vec2ForFramePrediction, COMMON VOICE DATA
+    # df_all_frames=pd.read_pickle('df_all_frames_commonvoice_en_dev_uk.pkl')
+
+    X, y = df_all_frames_to_X_y(df_all_frames)
+    model = Wav2Vec2ForFramePrediction('cmu', reducer=PCA(n_components=0.99, random_state=42), frame_classifier=KNeighborsClassifier(10, weights='distance', metric='cosine'))
+    model.fit(X, y)
+    model.save(name='model_commonvoice_pca_99_knn_us')
+
 
     # ----------------------------
 
@@ -543,28 +640,6 @@ if __name__ == '__main__':
 
 
 
-    # --------------- Inference demo --------------------
-    
-    # phoneme predictions on a train dataset with forced alignment
-    # comment for cmu or ipa
-    df_t_train, df_t_test = load_libri_dataset()
-    data = load_cmu_test_dataset(df_t_test)
-    # data = load_test_dataset(df_t_test)
-
-    # # phoneme predictions on a single audio sample with forced alignment
-    # pred = model.predict_with_timings(data.s.iloc[0], data.cmu_phones.iloc[0])
-    # prob_matrix = model.predict_phone_prob_matrix(data.s.iloc[0], 16000)
-
-    
-    from src.wav2vec2_frame_prediction import Wav2Vec2ForFramePrediction
-    default_model_cmu = Wav2Vec2ForFramePrediction('cmu')
-    default_model_cmu.load(name='model_mailabs_pca_0.95_knn_10_w')
-
-    # phoneme predictions on a single audio sample with forced alignment
-    pred = default_model_cmu.predict_with_timings(data.s.iloc[0], data.cmu_phones.iloc[0])
-    prob_matrix = default_model_cmu.predict_phone_prob_matrix(data.s.iloc[0], 16000)
-
-    phonetic_content = default_model_cmu.analyze_phonetic_content(data.s.iloc[0], data.cmu_phones.iloc[0])
     
     # default_model_ipa = Wav2Vec2ForFramePrediction('ipa')
     # default_model_ipa.load(name='model_mailabs_pca_95_knn_10_w_ipa')
