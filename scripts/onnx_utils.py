511b159d8f89a07cf035421a5e1bb2107b6e9388
Noé Tits <noe.tits@gmail.com>
2023-04-07 17:03:11 +0200

    move onnx utils to scripts

--- scripts/onnx_utils.py
+++ scripts/onnx_utils.py
@@ -5,9 +5,13 @@ from transformers import Wav2Vec2ForCTC
 import torch
 import argparse
 
-
+import pandas as pd
+import numpy as np
 import torch
 import torch.onnx
+
+import torch
+from collections import defaultdict
 
 # Original transformer model
 from transformers import Wav2Vec2Model, Wav2Vec2Processor, Wav2Vec2Config
@@ -25,6 +29,90 @@ class LastHiddenStateModel(torch.nn.Module):
         # input_values = self.processor(torch.tensor(s), sampling_rate=fs, return_tensors="pt").input_values.to('cpu')
         with torch.no_grad(): 
             return self.model(input).hidden_states[-1]
+
+
+def extract_linear_weights(model):
+    linear_weights = []
+    for name, module in model.named_children():
+        if isinstance(module, torch.nn.Linear):
+            linear_weights.append(module.weight.data)
+        linear_weights.extend(extract_linear_weights(module))
+    return linear_weights
+
+
+def extract_linear_bias(model):
+    linear_bias = []
+    for name, module in model.named_children():
+        if isinstance(module, torch.nn.Linear):
+            linear_bias.append(module.bias.data)
+        linear_bias.extend(extract_linear_bias(module))
+    return linear_bias
+
+# weights = extract_linear_weights(model)
+
+def extract_linear_layers(model):
+    linear_layers = []
+    for name, module in model.named_children():
+        if isinstance(module, torch.nn.Linear):
+            linear_layers.append((module, 'weight'))
+        linear_layers.extend(extract_linear_layers(module))
+    return linear_layers
+
+# https://pytorch.org/tutorials/intermediate/pruning_tutorial.html#pruning-a-module
+
+
+def prune_model(model, sparsity=0.7):
+    linear_layers = extract_linear_layers(model)
+
+    import torch.nn.utils.prune as prune
+    from torch.nn.utils.prune import remove
+
+    for l in linear_layers:
+        prune.ln_structured(l[0], name="weight", amount=sparsity, n=2, dim=0)
+    for l in linear_layers:
+        remove(l[0], name='weight')
+
+    # prune.global_unstructured(
+    #     linear_layers,
+    #     pruning_method=prune.L1Unstructured,
+    #     amount=sparsity,
+    # )
+    torch.save(model.state_dict(), 'pruned_model.pt')
+
+
+
+def explore_model_parameters(model):
+    total_n_parameters=sum(p.numel() for p in model.parameters())
+    module_types = defaultdict(int)
+    def count_parameters(model):
+        for name, module in model.named_children():
+            module_type = type(module).__name__
+            module_types[module_type] += sum(p.numel() for p in module.parameters())
+            count_parameters(module)
+
+    count_parameters(model)
+
+    module_types_nn= {m:module_types[m] for m in module_types if (m in torch.nn.__dict__ and m!='ModuleList')}
+    sorted_types = sorted(module_types_nn.items(), key=lambda x: x[1], reverse=True)    
+    total_n_parameters-sum([module_types_nn[m] for m in module_types_nn])
+
+    bias = extract_linear_bias(model)
+    all_bias=np.concatenate([w.flatten().numpy() for w in bias])
+
+    weights = extract_linear_weights(model)
+    weights_matrices_shapes=[w.shape for w in weights]
+    weights_matrices_abs_sum=[w.abs().sum().numpy()[()] for w in weights]
+    weights_matrices_squared_sum=[(w*w).sum().numpy()[()] for w in weights]
+    all_weights=np.concatenate([w.flatten().numpy() for w in weights])
+
+    distrib_df_abs=pd.DataFrame(np.histogram(weights_matrices_abs_sum, bins=20)).T
+    distrib_df_abs[int(len(distrib_df_abs)/2-10):int(len(distrib_df_abs)/2+10)]
+
+    distrib_df_squared=pd.DataFrame(np.histogram(weights_matrices_squared_sum, bins=20)).T
+
+    distrib_df_values=pd.DataFrame(np.histogram(all_weights, bins=100)).T
+    distrib_df_values[int(len(distrib_df_values)/2-10):int(len(distrib_df_values)/2+10)]
+    
 
 
 def convert_to_onnx(model_id_or_path, onnx_model_name):
