accf9279f7996a03641f726192ae12aca55b0b97
Noé Tits <noe.tits@gmail.com>
2023-08-01 11:01:43 +0200

    clone streamlit_audio_recorder components for demo, and copy files at the right place as explained in the repo

--- content_tools/run_content_tools.sh
+++ content_tools/run_content_tools.sh
@@ -1,2 +1,3 @@
-cron
-streamlit run content_tools/Hello.py --server.port 8501 --server.enableCORS=false --server.enableXsrfProtection=false
+# this gets the audio recorder component and puts it at the root of content_tools. If already cloned, we go in it and pull, then out of it. We copy only if not already existing
+cd content_tools && (git clone https://github.com/stefanrmmr/streamlit_audio_recorder  2> /dev/null || (cd "streamlit_audio_recorder" && git pull && cd ..)) && cp -rvn streamlit_audio_recorder/st_audiorec . && cp -vn streamlit_audio_recorder/st_custom_components.py . && cd ..
+streamlit run content_tools/Hello.py --server.port 8000 --server.enableCORS=false --server.enableXsrfProtection=false
