7f1065800a9cf64586bd8f600208bb41f07769fa
Noé Tits <noe.tits@gmail.com>
2023-08-01 10:38:21 +0200

    Remove default streamlit links, remove the check_password on the hello page. We check that in each page instead

--- content_tools/Hello.py
+++ content_tools/Hello.py
@@ -26,35 +26,22 @@ from utils import check_password
 
 def run():
     st.set_page_config(
-        page_title="Hello",
+        page_title="Content tools",
         page_icon="👋",
     )
 
-    st.write("# Welcome to Streamlit! 👋")
+    st.write("# Welcome to content-tools! 👋")
 
-
-    
-    st.sidebar.success("Select a demo above.")
+    st.sidebar.success("Select a tool above.")
 
     st.markdown(
         """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-        **👈 Select a demo from the sidebar** to see some examples
-        of what Streamlit can do!
-        ### Want to learn more?
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-        ### See more complex demos
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+        This site contains tools and demo related to Speech & Language processing to be used internally
+        **👈 Select a tool from the sidebar** to see some examples
     """
     )
 
 
 if __name__ == "__main__":
-    if check_password():
-      run()
+    # if check_password():
+    run()
