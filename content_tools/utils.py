62af7fc2637279f7742623a507370261397dc55c
Sebastien Wertz <sebastien.wertz@b12-consulting.com>
2024-02-12 13:36:03 +0100

    Change content-tools password autofill

--- content_tools/utils.py
+++ content_tools/utils.py
@@ -42,18 +42,18 @@ def check_password():
         else:
             st.session_state["password_correct"] = False
 
-    if "password_correct" not in st.session_state:
+    if not st.session_state.get("password_correct"):
         # First run, show input for password.
         st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
+            "Password",
+            type="password",
+            on_change=password_entered,
+            key="password",
+            autocomplete="current-password",
         )
-        return False
-    elif not st.session_state["password_correct"]:
-        # Password not correct, show input + error.
-        st.text_input(
-            "Password", type="password", on_change=password_entered, key="password"
-        )
-        st.error("😕 Password incorrect")
+        if st.session_state.get("password_correct") is False:
+            # Password not correct, show input + error.
+            st.error("😕 Password incorrect")
         return False
     else:
         # Password correct.
