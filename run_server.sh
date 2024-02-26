42da7a687a26a10fb05f928f9371b1b32105a9d0
Sebastien Wertz <sebastien.wertz@b12-consulting.com>
2024-02-26 15:12:51 +0100

    Add production.yml file for docker compose

--- run_server.sh
+++ run_server.sh
@@ -2,5 +2,5 @@
 #sudo mkdir -p hf_models/
 #sudo cp /home/mambauser/hf_models/last_hidden_state.quant.onnx ./hf_models/last_hidden_state.quant.onnx
 mfa server start
-gunicorn -w 2 -b 0.0.0.0:8000 'app:init_app()' --timeout 90
+gunicorn -w ${NUM_SERVICES:-1} -b 0.0.0.0:8000 'app:init_app()' --timeout 90
 
